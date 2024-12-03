21db5cbb99f53f2c75a6405453695969d7175556
Paul Tsai <paul_tsai@phison.com>
2024-12-03 14:03:02 +0800

    chore: init layout

--- cmd/openhdc/main.go
+++ cmd/openhdc/main.go
@@ -9,9 +9,24 @@ import (
 	"github.com/openhdc/openhdc/internal/cmd"
 )
 
-var version = "devel"
+var (
+	name    = "openhdc"
+	version = "devel"
+)
+
+var (
+// id      = uuid.New().String()
+// host, _ = os.Hostname()
+)
 
 func main() {
+	// new logger
+
+	// set global
+
+	// wire app
+
+	// start and wait for stop signal
 	signals := []os.Signal{syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM}
 	ctx, stop := signal.NotifyContext(context.Background(), signals...)
 	defer stop()
---
+++ connector/source/postgresql/go.mod
@@ -0,0 +1,3 @@
+module github.com/openhdc/openhdc/connector/source/postgresql
+
+go 1.23.3
