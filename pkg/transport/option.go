b5015e6f7f80e81c7032db57978a947a387f63af
Paul Tsai <paul_tsai@phison.com>
2024-12-04 09:27:56 +0800

    refactor: use transport as grpc

--- pkg/transport/option.go
+++ pkg/transport/option.go
@@ -1,3 +1,28 @@
 package transport
 
-type Option func(o *Server)
+import (
+	"google.golang.org/grpc"
+)
+
+type ServerOption func(*Server)
+
+func Network(network string) ServerOption {
+	return func(s *Server) {
+		s.network = network
+	}
+}
+
+func Address(address string) ServerOption {
+	return func(s *Server) {
+		s.address = address
+	}
+}
+
+func Connector() ServerOption {
+	return func(s *Server) {
+		s.grpcOpts = append(s.grpcOpts,
+			grpc.MaxRecvMsgSize(maxMsgSize),
+			grpc.MaxSendMsgSize(maxMsgSize),
+		)
+	}
+}
