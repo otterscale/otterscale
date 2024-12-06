04c51fbf8a90a8064e02e5bd24918fad990a3cdb
Paul Tsai <paul_tsai@phison.com>
2024-12-06 16:26:19 +0800

    feat: add postgres connector (wip)

---
+++ connector/postgresql/client/client.go
@@ -0,0 +1,69 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/openhdc/openhdc/pkg/adapter"
+)
+
+const (
+	defaultBatchSize      = 10000
+	defaultBatchSizeBytes = 100000000
+	defaultBatchTimeout   = 60 * time.Second
+)
+
+type Client struct {
+	batchSize      int64
+	batchSizeBytes int64
+	batchTimeout   time.Duration
+	createIndex    bool
+
+	connString string
+	config     *pgxpool.Config
+	pool       *pgxpool.Pool
+}
+
+func New(opts ...Option) (adapter.Adapter, error) {
+	c := &Client{
+		batchSize:      defaultBatchSize,
+		batchSizeBytes: defaultBatchSizeBytes,
+		batchTimeout:   defaultBatchTimeout,
+		createIndex:    true,
+	}
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	var err error
+	c.config, err = pgxpool.ParseConfig(c.connString)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c.pool, err = pgxpool.NewWithConfig(ctx, c.config)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *Client) TestConnection(ctx context.Context) error {
+	conn, err := c.pool.Acquire(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Release()
+
+	err = conn.Ping(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ connector/postgresql/client/codec.go
@@ -0,0 +1,92 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/apache/arrow-go/v18/arrow"
+	"github.com/apache/arrow-go/v18/arrow/array"
+	"github.com/apache/arrow-go/v18/arrow/memory"
+	"github.com/jackc/pgx/v5/pgtype"
+
+	"github.com/openhdc/openhdc/pkg/codec"
+)
+
+var _ codec.Codec = (*postgresqlCodec)(nil)
+
+type postgresqlCodec struct {
+	codec.DefaultCodec
+}
+
+func NewCodec() codec.Codec {
+	return &postgresqlCodec{}
+}
+
+func (c *postgresqlCodec) newArray(dt arrow.DataType, val any) (arrow.Array, error) {
+	b := array.NewBuilder(memory.DefaultAllocator, dt)
+	defer b.Release()
+	if val != nil {
+		if err := c.Append(b, val); err != nil {
+			return nil, err
+		}
+	}
+	return b.NewArray(), nil
+}
+
+//nolint:gocyclo
+func (c *postgresqlCodec) Encode(typ, val any) (arrow.Array, error) {
+	switch typ.(type) {
+	case *pgtype.ArrayCodec:
+	case pgtype.BitsCodec:
+	case pgtype.BoolCodec:
+		return c.newArray(arrow.FixedWidthTypes.Boolean, val)
+
+	case pgtype.BoxCodec:
+	case pgtype.ByteaCodec:
+	case pgtype.CircleCodec:
+	case *pgtype.CompositeCodec:
+	case pgtype.DateCodec:
+	case *pgtype.DateCodec:
+	case *pgtype.EnumCodec:
+	case pgtype.Float4Codec:
+	case pgtype.Float8Codec:
+	case pgtype.HstoreCodec:
+	case pgtype.InetCodec:
+	case pgtype.Int2Codec:
+	case pgtype.Int4Codec:
+	case pgtype.Int8Codec:
+		return c.newArray(arrow.PrimitiveTypes.Int64, val)
+
+	case pgtype.IntervalCodec:
+	case *pgtype.JSONCodec:
+	case *pgtype.JSONBCodec:
+	case pgtype.LineCodec:
+	case pgtype.LsegCodec:
+	case pgtype.LtreeCodec:
+	case pgtype.MacaddrCodec:
+	case *pgtype.MultirangeCodec:
+	case pgtype.NumericCodec:
+	case pgtype.PathCodec:
+	case pgtype.PointCodec:
+	case pgtype.PolygonCodec:
+	case pgtype.QCharCodec:
+	case *pgtype.RangeCodec:
+	case pgtype.RecordCodec:
+	case pgtype.TextCodec:
+		return c.newArray(arrow.BinaryTypes.String, val)
+
+	case *pgtype.TextFormatOnlyCodec:
+	case pgtype.TIDCodec:
+	case pgtype.TimeCodec:
+	case *pgtype.TimestampCodec:
+	case *pgtype.TimestamptzCodec:
+		return c.newArray(arrow.FixedWidthTypes.Timestamp_us, val)
+
+	case pgtype.Uint32Codec:
+	// FIXME: github.com/jackc/pgx/v5 v5.7.1.next
+	// case pgtype.Uint64Codec:
+	case pgtype.UUIDCodec:
+	case *pgtype.XMLCodec:
+	}
+
+	return nil, fmt.Errorf("type %T: %w", typ, codec.ErrNotSupported)
+}
