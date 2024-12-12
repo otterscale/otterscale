e154257af9de4248d5e10027d73791ad7b549a9a
Paul Tsai <paul_tsai@phison.com>
2024-12-12 19:24:56 +0800

    feat: postgres migration (wip)

--- connector/postgresql/client/pg.go
+++ connector/postgresql/client/pg.go
@@ -2,6 +2,9 @@ package client
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/apache/arrow-go/v18/arrow"
 	"github.com/jackc/pgx/v5"
@@ -50,25 +53,78 @@ order by a.attnum
 	return pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[pgAttribute])
 }
 
-func toFieldMetadata(att *pgAttribute, dataType string) arrow.Metadata {
-	m := map[string]string{
-		metadata.KeyFieldDataType: dataType,
+func createTableIfNotExists(ctx context.Context, pool *pgxpool.Pool, sch *arrow.Schema) error {
+	tableName, ok := sch.Metadata().GetValue(metadata.KeySchemaTableName)
+	if !ok {
+		return errors.New("table name not found")
 	}
-	if att.ConType != nil && att.ConName != nil {
-		switch *att.ConType {
-		case 'p':
-			m[metadata.KeyFieldPrimaryKeyName] = *att.ConName
-		case 'u':
-			m[metadata.KeyFieldUniqueName] = *att.ConName
+
+	tableName += "_tmp"
+
+	columns := []string{}
+	pks := []string{}
+	for _, field := range sch.Fields() {
+		name := sanitize(field.Name)
+
+		dataType, ok := field.Metadata.GetValue(metadata.KeyFieldDataType)
+		if !ok {
+			return fmt.Errorf("data type not found: %s", name)
+		}
+
+		unique := ""
+		if _, ok := field.Metadata.GetValue(metadata.KeyFieldIsUnique); ok {
+			unique = "unique"
+		}
+
+		null := ""
+		if !field.Nullable {
+			null = "not null"
+		}
+
+		column := fmt.Sprintf("%s %s %s %s", name, dataType, unique, null)
+		columns = append(columns, column)
+
+		if _, ok := field.Metadata.GetValue(metadata.KeyFieldIsPrimaryKey); ok {
+			pks = append(pks, name)
 		}
 	}
-	return arrow.MetadataFrom(m)
+
+	var b strings.Builder
+	b.WriteString("create table if not exists ")
+	b.WriteString(tableName)
+	b.WriteString(" (")
+
+	b.WriteString(strings.Join(columns, ", "))
+
+	if len(pks) > 0 {
+		b.WriteString(", constraint ")
+		b.WriteString(tableName)
+		b.WriteString("_pkey")
+		b.WriteString(" primary key (")
+		b.WriteString(strings.Join(pks, ", "))
+		b.WriteString(")")
+	}
+
+	b.WriteString(")")
+
+	if _, err := pool.Exec(ctx, b.String()); err != nil {
+		return err
+	}
+	return nil
 }
 
-func toSchemaMetadata(tableName string) *arrow.Metadata {
-	m := map[string]string{
-		metadata.KeySchemaTableName: tableName,
+func dropTable(ctx context.Context, pool *pgxpool.Pool, sch *arrow.Schema) error {
+	tableName, ok := sch.Metadata().GetValue(metadata.KeySchemaTableName)
+	if !ok {
+		return errors.New("table name not found")
 	}
-	md := arrow.MetadataFrom(m)
-	return &md
+
+	var b strings.Builder
+	b.WriteString("drop table ")
+	b.WriteString(tableName)
+
+	if _, err := pool.Exec(ctx, b.String()); err != nil {
+		return err
+	}
+	return nil
 }
