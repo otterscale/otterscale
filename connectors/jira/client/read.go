f48d3e30ba5b82dc8da1f78552bb8602a7a77431
WoodyLin <woody_lin@phison.com>
2025-01-09 18:03:00 +0800

    feat: add JIRA get multi project data and batch insert

--- connectors/jira/client/read.go
+++ connectors/jira/client/read.go
@@ -2,46 +2,53 @@ package client
 
 import (
 	"context"
-	"fmt"
-	"time"
+	"strings"
 
 	jira "github.com/andygrunwald/go-jira"
 	"github.com/openhdc/openhdc"
 	pb "github.com/openhdc/openhdc/api/connector/v1"
-	"github.com/openhdc/openhdc/api/property/v1"
 )
 
-func (c *Client) Read(ctx context.Context, msg chan<- *pb.Message, opts openhdc.ReadOptions) error {
-	// sync issues table
-	err := c.readIssue(msg)
+func (c *Client) Read(ctx context.Context, msgs chan<- *pb.Message, rdr *openhdc.Reader) error {
+	var err error
+	for _, pj := range c.opts.projects {
+		// sync issues table
+		err = c.readIssue(pj, c.opts.startDate, msgs, rdr)
+	}
 
 	return err
 }
 
-func (c *Client) readIssue(msg chan<- *pb.Message) error {
-	// TODO: change setting
-	batchSize := 100 // 每次抓取的 Issue 數量
-	startAt := 0     // 起始位置
-	projectName := "[System] IFAS"
-
-	// timestamp
-	syncedAt := time.Now().UTC().Truncate(time.Second)
-
+func (c *Client) readIssue(pj string, sd string, msgs chan<- *pb.Message, rdr *openhdc.Reader) error {
 	// migration
-	new, err := pb.NewMessage(property.MessageKind_migrate, c.issueSchema.Record(), c.opts.name, syncedAt)
-	if err != nil {
+	if err := rdr.Send(pb.Migrate, msgs, c.issueSchema.Record()); err != nil {
 		return err
 	}
-	msg <- new
+
+	// jql
+	var jql strings.Builder
+	jql.WriteString("project = '")
+	jql.WriteString(pj)
+	jql.WriteString("'")
+	if sd != "" {
+		jql.WriteString(" AND created >= '")
+		jql.WriteString(sd)
+		jql.WriteString("'")
+	}
 
 	// read jira issues
+	var (
+		count     int64
+		startAt   = 0
+		batchSize = 50
+	)
 	for {
 		options := &jira.SearchOptions{
 			StartAt:    startAt,
 			MaxResults: batchSize,
 		}
 		// get issues
-		issues, resp, err := c.jiraClient.Issue.Search(fmt.Sprintf("project = '%s'", projectName), options)
+		issues, resp, err := c.jiraClient.Issue.Search(jql.String(), options)
 		if err != nil {
 			return err
 		}
@@ -49,14 +56,16 @@ func (c *Client) readIssue(msg chan<- *pb.Message) error {
 		// append issue
 		for _, issue := range issues {
 			c.issueSchema.Append(&issue)
+			count++
 		}
 
 		// new message
-		new, err := pb.NewMessage(property.MessageKind_insert, c.issueSchema.Record(), c.opts.name, syncedAt)
-		if err != nil {
-			return err
+		if count >= rdr.BatchSize() {
+			if err := rdr.Send(pb.Insert, msgs, c.issueSchema.Record()); err != nil {
+				return err
+			}
+			count = 0
 		}
-		msg <- new
 
 		// check is there next page
 		if resp.StartAt+resp.MaxResults >= resp.Total {
@@ -67,5 +76,12 @@ func (c *Client) readIssue(msg chan<- *pb.Message) error {
 		startAt = resp.StartAt + resp.MaxResults
 	}
 
-	return err
+	// remain
+	if count > 0 {
+		if err := rdr.Send(pb.Insert, msgs, c.issueSchema.Record()); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
