d5a5476581ee4e43ee7c81bd8056efc8bf31a68e
Paul Tsai <paul_tsai@phison.com>
2025-02-20 13:26:28 +0800

    chore: add blank line for improved code readability in grpc.go

--- internal/service/grpc.go
+++ internal/service/grpc.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/google/wire"
+
 	"github.com/openhdc/openhdc"
 	v1 "github.com/openhdc/openhdc/api/user/v1"
 	"github.com/openhdc/openhdc/internal/service/app"
