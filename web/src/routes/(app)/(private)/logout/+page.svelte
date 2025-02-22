d4e7afab9aac3df11ef4de1876b4497fa703de9a
CH Tsai <zx.c@nycu.edu.tw>
2025-02-22 21:01:26 +0800

    feat: implement logout functionality with countdown redirect and update authentication checks

---
+++ web/src/routes/(app)/(private)/logout/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import pb from '$lib/pb';
+
+	import { onMount } from 'svelte';
+
+	let countdown = 3;
+
+	onMount(() => {
+		pb.authStore.clear();
+
+		const timer = setInterval(() => {
+			countdown--;
+			if (countdown <= 0) {
+				clearInterval(timer);
+				goto('/');
+			}
+		}, 1000);
+
+		return () => clearInterval(timer);
+	});
+
+	var color = [
+		'from-red-500 to-orange-300/80',
+		'from-blue-500 to-purple-300/80',
+		'from-green-500 to-teal-300/80',
+		'from-pink-500 to-rose-300/80',
+		'from-yellow-500 to-amber-300/80'
+	][Math.floor(Math.random() * 5)];
+</script>
+
+<div class="flex min-h-[calc(100vh_-_theme(spacing.16))] w-full items-center justify-center">
+	<div class="flex flex-col items-center gap-4">
+		<span
+			class="pointer-events-none flex justify-center whitespace-pre-wrap bg-gradient-to-b bg-clip-text text-center text-4xl font-semibold leading-none text-transparent {color}"
+		>
+			Good bye!
+		</span>
+		<span class="text-sm text-muted-foreground">Redirecting in {countdown} seconds...</span>
+	</div>
+</div>
