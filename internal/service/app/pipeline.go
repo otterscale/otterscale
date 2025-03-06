5213ca9b262b518d1935bec2e2053a91a3ffbfa9
Paul Tsai <paul_tsai@phison.com>
2025-03-06 10:42:13 +0800

    feat: implement CRUD operations for CronJob and Job models in Kubernetes service

--- internal/service/app/pipeline.go
+++ internal/service/app/pipeline.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 
 	"github.com/openhdc/openhdc/internal/service/domain/service"
@@ -20,10 +22,11 @@ func NewPipelineApp(svc *service.KubeService) *PipelineApp {
 
 func (a *PipelineApp) Bind(app core.App) {
 	app.OnServe().BindFunc(a.onServe)
-	app.OnRecordAfterCreateSuccess("pipelines").BindFunc(a.onRecordAfterCreateSuccess)
+	app.OnRecordCreateRequest("pipelines").BindFunc(a.onRecordCreateRequest)
+	app.OnRecordUpdateRequest("pipelines").BindFunc(a.onRecordUpdateRequest)
+	app.OnRecordDeleteRequest("pipelines").BindFunc(a.onRecordDeleteRequest)
 }
 
-// TODO ADD AUTH
 func (a *PipelineApp) onServe(se *core.ServeEvent) error {
 	g := se.Router.Group("/api/hdc/pipeline")
 
@@ -44,22 +47,72 @@ func (a *PipelineApp) onServe(se *core.ServeEvent) error {
 	})
 
 	g.POST("/cron-job/{name}/job", func(e *core.RequestEvent) error {
-		data := struct {
-			CreatedBy string `json:"createdBy" form:"createdBy"`
-		}{}
-		if err := e.BindBody(&data); err != nil {
-			return e.BadRequestError("Failed to read request data", err)
-		}
-		if err := a.svc.CreateJobFromCronJob(e.Request.Context(), e.Request.PathValue("name"), data.CreatedBy); err != nil {
+		j, err := a.svc.CreateJobFromCronJob(e.Request.Context(), e.Request.PathValue("name"), e.Auth.FieldsData()["name"].(string))
+		if err != nil {
 			return err
 		}
-		return e.JSON(http.StatusCreated, empty{})
-	})
-	//.Bind(apis.RequireAuth())
+		return e.JSON(http.StatusCreated, j)
+	}).Bind(apis.RequireAuth())
 
 	return se.Next()
 }
 
-func (a *PipelineApp) onRecordAfterCreateSuccess(e *core.RecordEvent) error {
-	return a.svc.CreateCronJob(e.Context, "hello", "busybox:1.28", "* * * * *")
+func (a *PipelineApp) onRecordCreateRequest(e *core.RecordRequestEvent) error {
+	ctx := e.Request.Context()
+	name := e.Record.GetString("name")
+	image := e.Record.GetString("image")
+	schedule := e.Record.GetString("schedule")
+
+	if strings.ToLower(name) != name {
+		return e.BadRequestError("Name must be lowercase", nil)
+	}
+	if image == "" {
+		return e.BadRequestError("Image must be provided", nil)
+	}
+	if schedule == "" {
+		return e.BadRequestError("Schedule must be provided", nil)
+	}
+
+	if _, err := a.svc.CreateCronJob(ctx, name, image, schedule); err != nil {
+		return e.InternalServerError("Failed to create cron job", err)
+	}
+
+	return e.Next()
+}
+
+func (a *PipelineApp) onRecordUpdateRequest(e *core.RecordRequestEvent) error {
+	ctx := e.Request.Context()
+	name := e.Record.GetString("name")
+	image := e.Record.GetString("image")
+	schedule := e.Record.GetString("schedule")
+
+	if strings.ToLower(name) != name {
+		return e.BadRequestError("Name must be lowercase", nil)
+	}
+	if image == "" {
+		return e.BadRequestError("Image must be provided", nil)
+	}
+	if schedule == "" {
+		return e.BadRequestError("Schedule must be provided", nil)
+	}
+
+	if _, err := a.svc.UpdateCronJob(ctx, name, image, schedule); err != nil {
+		return e.InternalServerError("Failed to update cron job", err)
+	}
+
+	return e.Next()
+}
+
+func (a *PipelineApp) onRecordDeleteRequest(e *core.RecordRequestEvent) error {
+	ctx := e.Request.Context()
+	name := e.Record.GetString("name")
+
+	if strings.ToLower(name) != name {
+		return e.BadRequestError("Name must be lowercase", nil)
+	}
+	if err := a.svc.DeleteCronJob(ctx, name); err != nil {
+		return e.InternalServerError("Failed to delete cron job", err)
+	}
+
+	return e.Next()
 }
--- internal/service/domain/model/job.go
+++ internal/service/domain/model/job.go
@@ -1,13 +1,15 @@
 package model
 
-import "time"
+import (
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type Job struct {
 	UID                     string       `json:"uid"`
 	Name                    string       `json:"name"`
 	Succeeded               bool         `json:"succeeded"`
-	StartTime               time.Time    `json:"startTime"`
-	CompletionTime          time.Time    `json:"completionTime"`
+	StartTime               *v1.Time     `json:"startTime"`
+	CompletionTime          *v1.Time     `json:"completionTime"`
 	TTLSecondsAfterFinished *int32       `json:"ttlSecondsAfterFinished"`
 	Containers              []*Container `json:"containers"`
 }
