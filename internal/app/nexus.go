cc72a32db94f0e8c3eb495e8368d66ea621a1598
Paul Tsai <paul_tsai@phison.com>
2025-04-16 11:18:43 +0800

    feat: add Scope message and implement List/Create scope methods in Nexus service

--- internal/app/nexus.go
+++ internal/app/nexus.go
@@ -1,13 +1,8 @@
 package app
 
 import (
-	"context"
-
-	"connectrpc.com/connect"
-	pb "github.com/openhdc/openhdc/api/nexus/v1"
 	"github.com/openhdc/openhdc/api/nexus/v1/pbconnect"
 
-	"github.com/openhdc/openhdc/internal/domain/model"
 	"github.com/openhdc/openhdc/internal/domain/service"
 )
 
@@ -21,88 +16,3 @@ func NewNexusApp(svc *service.NexusService) *NexusApp {
 }
 
 var _ pbconnect.NexusHandler = (*NexusApp)(nil)
-
-func (a *NexusApp) GetConfiguration(ctx context.Context, req *connect.Request[pb.GetConfigurationRequest]) (*connect.Response[pb.Configuration], error) {
-	cfg, err := a.svc.GetConfiguration(ctx)
-	if err != nil {
-		return nil, err
-	}
-	res := toProtoConfiguration(cfg)
-	return connect.NewResponse(res), nil
-}
-
-func (a *NexusApp) UpdateNTPServer(ctx context.Context, req *connect.Request[pb.UpdateNTPServerRequest]) (*connect.Response[pb.Configuration_NTPServer], error) {
-	ntpServers, err := a.svc.UpdateNTPServer(ctx, req.Msg.GetAddresses())
-	if err != nil {
-		return nil, err
-	}
-	res := toProtoNTPServer(ntpServers)
-	return connect.NewResponse(res), nil
-}
-
-func (a *NexusApp) UpdatePackageRepository(ctx context.Context, req *connect.Request[pb.UpdatePackageRepositoryRequest]) (*connect.Response[pb.Configuration_PackageRepository], error) {
-	pr, err := a.svc.UpdatePackageRepository(ctx, int(req.Msg.GetId()), req.Msg.GetUrl(), req.Msg.GetSkipJuju())
-	if err != nil {
-		return nil, err
-	}
-	res := toProtoPackageRepository(pr)
-	return connect.NewResponse(res), nil
-}
-
-func (a *NexusApp) UpdateDefaultBootResource(ctx context.Context, req *connect.Request[pb.UpdateDefaultBootResourceRequest]) (*connect.Response[pb.Configuration_BootResource], error) {
-	br, err := a.svc.UpdateDefaultBootResource(ctx, req.Msg.GetDistroSeries())
-	if err != nil {
-		return nil, err
-	}
-	res := toProtoBootResource(br)
-	return connect.NewResponse(res), nil
-}
-
-func toProtoNTPServer(addresses []string) *pb.Configuration_NTPServer {
-	ret := &pb.Configuration_NTPServer{}
-	ret.SetAddresses(addresses)
-	return ret
-}
-
-func toProtoPackageRepositories(prs []*model.PackageRepository) []*pb.Configuration_PackageRepository {
-	ret := []*pb.Configuration_PackageRepository{}
-	for _, pr := range prs {
-		ret = append(ret, toProtoPackageRepository(pr))
-	}
-	return ret
-}
-
-func toProtoPackageRepository(pr *model.PackageRepository) *pb.Configuration_PackageRepository {
-	ret := &pb.Configuration_PackageRepository{}
-	ret.SetId(int64(pr.ID))
-	ret.SetName(pr.Name)
-	ret.SetUrl(pr.URL)
-	ret.SetEnabled(pr.Enabled)
-	return ret
-}
-
-func toProtoBootResources(brs []*model.BootResource) []*pb.Configuration_BootResource {
-	ret := []*pb.Configuration_BootResource{}
-	for _, br := range brs {
-		ret = append(ret, toProtoBootResource(br))
-	}
-	return ret
-}
-
-func toProtoBootResource(br *model.BootResource) *pb.Configuration_BootResource {
-	ret := &pb.Configuration_BootResource{}
-	ret.SetName(br.Name)
-	ret.SetArchitecture(br.Architecture)
-	ret.SetStatus(br.Status)
-	ret.SetDefault(br.Default)
-	ret.SetDistroSeries(br.DistroSeries)
-	return ret
-}
-
-func toProtoConfiguration(c *model.Configuration) *pb.Configuration {
-	ret := &pb.Configuration{}
-	ret.SetNtpServer(toProtoNTPServer(c.NTPServers))
-	ret.SetPackageRepositories(toProtoPackageRepositories(c.PackageRepositories))
-	ret.SetBootResources(toProtoBootResources(c.BootResources))
-	return ret
-}
---
+++ internal/domain/model/scope.go
@@ -0,0 +1,5 @@
+package model
+
+import "github.com/juju/juju/api/base"
+
+type Scope = base.UserModelSummary
