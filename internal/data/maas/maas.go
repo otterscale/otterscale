502c09a9927ecbaf1d2094ffe89ea15450782cef
Paul Tsai <paul_tsai@phison.com>
2025-04-22 15:27:59 +0800

    refactor: update default key value for MAAS API to use placeholder

--- internal/data/maas/maas.go
+++ internal/data/maas/maas.go
@@ -10,12 +10,13 @@ type MAAS = client.Client
 
 const (
 	defaultURL     = "http://localhost:5240/MAAS/"
+	defaultKey     = "http://localhost:5240/MAAS/"
 	defaultVersion = "2.0"
 )
 
 func New() (*MAAS, error) {
 	url := env.GetOrDefault(env.OPENHDC_MAAS_API_URL, defaultURL)
-	key := env.GetOrDefault(env.OPENHDC_MAAS_API_KEY, "")
+	key := env.GetOrDefault(env.OPENHDC_MAAS_API_KEY, "::")
 	version := env.GetOrDefault(env.OPENHDC_MAAS_API_VERSION, defaultVersion)
 	return client.GetClient(url, key, version)
 }
