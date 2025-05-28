76544c86a3a42fe8d69c7a038a0ab4ed4747cea1
Paul Tsai <paul_tsai@phison.com>
2025-05-28 15:45:57 +0800

    feat: remove landing page

--- web/src/routes/(app)/(sidebar)/+layout.svelte
+++ web/src/routes/(app)/(sidebar)/+layout.svelte
@@ -6,7 +6,7 @@
 	import { AppSidebar } from '$lib/components/sidebar';
 	import { Avatar, FunctionBar, SearchBar } from '$lib/components/navbar';
 	import { capitalizeFirstLetter } from 'better-auth';
-	import type { PageData } from '../../$types';
+	import type { PageData } from './$types';
 	import type { Snippet } from 'svelte';
 	import { i18n } from '$lib/i18n';
 
