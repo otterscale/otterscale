b001799f069fd9b992e07b78ac6ea766da20033e
Paul Tsai <paul_tsai@phison.com>
2025-05-28 17:20:11 +0800

    feat: refactor login and signup forms, removing obsolete components and integrating new ones

---
+++ web/src/lib/components/login/signup-form.svelte
@@ -0,0 +1,138 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { writable } from 'svelte/store';
+	import { toast } from 'svelte-sonner';
+	import Icon from '@iconify/svelte';
+	import { Button, buttonVariants } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Label } from '$lib/components/ui/label';
+	import { Input } from '$lib/components/ui/input';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import { i18n } from '$lib/i18n';
+	import { cn } from '$lib/utils';
+	import { appendCallback, getCallback } from '$lib/callback';
+	import { signUp } from '$lib/auth-client';
+	import { goto, invalidate } from '$app/navigation';
+
+	let { class: className, ...restProps }: HTMLAttributes<HTMLDivElement> = $props();
+
+	const firstName = writable('');
+	const lastName = writable('');
+	const email = writable('');
+	const password = writable('');
+	const passwordConfirm = writable('');
+	const loading = writable(false);
+
+	async function onSubmit() {
+		if ($password !== $passwordConfirm) {
+			toast.error('Password confirmation does not match.');
+			return;
+		}
+
+		loading.set(true);
+
+		await signUp.email({
+			email: $email,
+			password: $password,
+			name: `${$firstName} ${$lastName}`,
+			fetchOptions: {
+				async onSuccess() {
+					await invalidate('app:user');
+					toast.success('Account created successfully! Please sign in to continue.');
+					goto(i18n.resolveRoute('/'));
+				},
+				onError(context) {
+					toast.error(context.error.message);
+				}
+			}
+		});
+
+		loading.set(false);
+	}
+</script>
+
+<div class={cn('flex w-full flex-col gap-6', className)} {...restProps}>
+	<Card.Root>
+		<Card.Header class="text-center">
+			<Card.Title class="text-xl">Sign up</Card.Title>
+			<Card.Description>Enter your information to create an account</Card.Description>
+		</Card.Header>
+		<Card.Content class="grid gap-6">
+			<form onsubmit={onSubmit}>
+				<div class="grid gap-2 space-y-2">
+					<div class="grid grid-cols-2 gap-4">
+						<div class="grid gap-2">
+							<Label for="first-name">First name</Label>
+							<Input
+								id="first-name"
+								placeholder="Max"
+								autocomplete="given-name"
+								disabled={$loading}
+								bind:value={$firstName}
+								required
+							/>
+						</div>
+						<div class="grid gap-2">
+							<Label for="last-name">Last name</Label>
+							<Input
+								id="last-name"
+								placeholder="Robinson"
+								autocomplete="family-name"
+								disabled={$loading}
+								bind:value={$lastName}
+								required
+							/>
+						</div>
+					</div>
+					<div class="grid gap-2">
+						<Label for="email">Email</Label>
+						<Input
+							id="email"
+							placeholder="name@example.com"
+							type="email"
+							autocapitalize="none"
+							autocomplete="email"
+							autocorrect="off"
+							disabled={$loading}
+							bind:value={$email}
+							required
+						/>
+					</div>
+					<div class="grid gap-2">
+						<Label for="password">Password</Label>
+						<Input
+							id="password"
+							placeholder="********"
+							type="password"
+							autocapitalize="none"
+							autocomplete="current-password"
+							disabled={$loading}
+							bind:value={$password}
+							required
+						/>
+					</div>
+					<div class="grid gap-2">
+						<Label for="password-confirm">Confirm password</Label>
+						<Input
+							id="password-confirm"
+							placeholder="********"
+							type="password"
+							autocapitalize="none"
+							autocomplete="current-password"
+							disabled={$loading}
+							bind:value={$passwordConfirm}
+							required
+						/>
+					</div>
+					<Button type="submit" class="[&_svg]:size-5" disabled={$loading}>
+						{#if $loading}
+							<Icon icon="ph:spinner-gap" class="animate-spin" />
+						{:else}
+							<p>Create an account</p>
+						{/if}
+					</Button>
+				</div>
+			</form>
+		</Card.Content>
+	</Card.Root>
+</div>
--- web/src/routes/(app)/(blank)/login/+page.svelte
+++ web/src/routes/(app)/(blank)/login/+page.svelte
@@ -1,45 +1,33 @@
 <script lang="ts">
-	import { UserAuthForm } from '$lib/components/login';
+	import * as Avatar from '$lib/components/ui/avatar';
 	import { Button } from '$lib/components/ui/button';
+	import { LoginForm } from '$lib/components/login';
 	import { appendCallback } from '$lib/callback';
 </script>
 
-<div
-	class="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
->
-	<div class="absolute right-4 top-4 flex md:right-8 md:top-8">
-		<Button href={appendCallback('/signup')} variant="ghost">Sign up</Button>
+<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+	<div class="flex w-full max-w-sm flex-col items-center gap-6">
+		<Avatar.Root class="size-24 shadow-xl">
+			<Avatar.Image src="/images/otterscale.jpeg" alt="@shadcn" />
+			<Avatar.Fallback>OS</Avatar.Fallback>
+		</Avatar.Root>
+		<LoginForm />
 	</div>
-	<div class="relative hidden h-full flex-col bg-muted p-10 dark:border-r lg:flex">
-		<img
-			src="/images/placeholder.svg"
-			alt="placeholder"
-			class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.6] dark:grayscale"
-		/>
-		<div class="relative z-20 flex items-center text-lg font-medium">
-			<img src="/images/phison.svg" alt="phison" class="w-28" />
-		</div>
+	<div class="space-y-1 text-center text-xs text-muted-foreground">
+		<p>By continuing, you agree to our</p>
+		<p>
+			<a href="/terms" class="underline underline-offset-4 hover:text-primary">
+				Terms of Service
+			</a>
+			and
+			<a href="/privacy" class="underline underline-offset-4 hover:text-primary">
+				Privacy Policy
+			</a>
+			.
+		</p>
 	</div>
-	<div class="p-8">
-		<div class="mx-auto flex w-full flex-col items-center justify-center space-y-6">
-			<div class="flex flex-col space-y-2 text-center">
-				<h1 class="text-2xl font-semibold tracking-tight">Sign In</h1>
-				<p class="text-sm text-muted-foreground">Enter your email to access your account</p>
-			</div>
-			<UserAuthForm />
-			<div class="space-y-1 text-center text-xs text-muted-foreground">
-				<p>By continuing, you agree to our</p>
-				<p>
-					<a href="/terms" class="underline underline-offset-4 hover:text-primary">
-						Terms of Service
-					</a>
-					and
-					<a href="/privacy" class="underline underline-offset-4 hover:text-primary">
-						Privacy Policy
-					</a>
-					.
-				</p>
-			</div>
-		</div>
-	</div>
+</div>
+
+<div class="absolute right-4 top-4 flex md:right-8 md:top-8">
+	<Button href={appendCallback('/signup')} variant="ghost">Sign up</Button>
 </div>
