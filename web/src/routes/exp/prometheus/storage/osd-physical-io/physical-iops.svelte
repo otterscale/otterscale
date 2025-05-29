24260f2578918629ce12b2b66d2807b09f920bd1
Zhang, En-Yao <enyao_chang@phison.com>
2025-05-29 10:41:13 +0800

    refactor: simplify the directory structure of Ceph dashboard

---
+++ web/src/routes/exp/prometheus/storage/osd-physical-io/physical-iops.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { PrometheusDriver } from 'prometheus-query';
+	import ComponentLoading from '$lib/components/otterscale/ui/component-loading.svelte';
+	import type { Scope } from '$gen/api/scope/v1/scope_pb';
+	import NoData from '../../utils/empty.svelte';
+
+	let { client, scope: scope }: { client: PrometheusDriver; scope: Scope } = $props();
+	const query = $derived(
+		`
+        sum(
+            (
+                rate(node_disk_reads_completed{juju_model_uuid=~"${scope.uuid}"}[4m])
+            or
+                rate(node_disk_reads_completed_total{juju_model_uuid=~"${scope.uuid}"}[4m])
+            )
+        +
+            (
+                rate(node_disk_writes_completed{juju_model_uuid=~"${scope.uuid}"}[4m])
+            or
+                rate(node_disk_writes_completed_total{juju_model_uuid=~"${scope.uuid}"}[4m])
+            )
+        )
+		`
+	);
+</script>
+
+{#await client.instantQuery(query)}
+	<ComponentLoading />
+{:then response}
+	{@const results = response.result}
+	{#if results.length === 0}
+		<NoData />
+	{:else}
+		{@const [result] = results}
+		{@const iops = result.value.value}
+		<p class="text-5xl">{Math.round(iops)}</p>
+	{/if}
+{:catch error}
+	Error
+{/await}
