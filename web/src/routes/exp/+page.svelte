c1ee7471800937a9bd21213bfb7ac41a97d24d07
ZhangEnYao <76697563+ZhangEnYao@users.noreply.github.com>
2025-06-19 17:04:49 +0800

    feat(alert): implement single and multiple alert components with context management (#91)

--- web/src/routes/exp/+page.svelte
+++ web/src/routes/exp/+page.svelte
@@ -1,36 +1,63 @@
-<script>
-	import { onMount } from 'svelte';
+<script lang="ts">
+	import {
+		Single as SingleAlert,
+		Multiple as MultipleAlert
+	} from '$lib/components/custom/alert/index';
 
-	let title = 'Title 1';
+	const alert: SingleAlert.AlertType = {
+		level: 'ERROR',
+		title: 'Need Help?',
+		message:
+			'Visit our documentation or join the community forum for detailed guides, troubleshooting tips, and to connect with other Svelte developers. Our support resources are available 24/7 to assist you with any questions or issues you may encounter.',
+		action: () => console.log('Need Help?')
+	};
 
-	onMount(() => {
-		window.addEventListener('scroll', handleScroll);
-	});
-
-	function handleScroll() {
-		const section1 = document.getElementById('section1');
-		const section2 = document.getElementById('section2');
-
-		const section1Top = section1?.getBoundingClientRect().top ?? 0;
-		const section2Top = section2?.getBoundingClientRect().top ?? 0;
-
-		if (section2Top <= 0) {
-			title = 'Title 2';
-		} else if (section1Top <= 0) {
-			title = 'Title 1';
+	const alerts: SingleAlert.AlertType[] = [
+		{
+			level: 'DEBUG',
+			title: 'Welcome to Svelte Lab!',
+			message: 'Explore Svelte features and components.',
+			action: () => console.log('Welcome to Svelte Lab!')
+		},
+		{
+			level: 'INFORMATION',
+			title: 'Svelte Lab is Ready',
+			message:
+				'This is a development environment for Svelte. You can test components, styles, and functionality here before deploying to production.',
+			action: () => console.log('Svelte Lab is Ready')
+		},
+		{
+			level: 'ERROR',
+			title: 'Need Help?erfgjwnsdlcfvher;ifjsn/dlvijhsn.dlk/fjnws.delcfjwnsed.fkwnef.',
+			message:
+				'Visit our documentation or join the community forum for detailed guides, troubleshooting tips, and to connect with other Svelte developers. Our support resources are available 24/7 to assist you with any questions or issues you may encounter.',
+			action: () => console.log('Need Help?')
+		}
+	];
+	function variantGetter(level: string) {
+		switch (level) {
+			case 'INFORMATION':
+				return 'information';
+			case 'ERROR':
+				return 'destructive';
+			default:
+				return 'default';
 		}
 	}
+	let index: number = $state(0);
 </script>
 
-<div class="bg-muted/60 fixed top-0 z-50 m-4 w-full rounded-lg p-4 text-center">
-	{title}
-</div>
+<SingleAlert.Root>
+	<SingleAlert.Icon />
+	<SingleAlert.Title>{alert.title}</SingleAlert.Title>
+	<SingleAlert.Description>{alert.message}</SingleAlert.Description>
+	<SingleAlert.Action onclick={alert.action}>Action</SingleAlert.Action>
+</SingleAlert.Root>
 
-<div id="section1" class="h-screen p-4">
-	<h2>Title 1</h2>
-	<p>Content for section 1...</p>
-</div>
-<div id="section2" class="h-screen p-4">
-	<h2>Title 2</h2>
-	<p>Content for section 2...</p>
-</div>
+<MultipleAlert.Root {alerts} bind:index duration={2000} {variantGetter}>
+	<MultipleAlert.Icon />
+	<MultipleAlert.Controller />
+	<MultipleAlert.Title>{alerts[index].title}</MultipleAlert.Title>
+	<MultipleAlert.Description>{alerts[index].message}</MultipleAlert.Description>
+	<MultipleAlert.Action onclick={alerts[index].action}>Action</MultipleAlert.Action>
+</MultipleAlert.Root>
