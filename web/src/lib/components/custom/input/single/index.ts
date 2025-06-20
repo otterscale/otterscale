6cdcdcbc8a2085cad5e3d69c29aa41a4e38da751
ZhangEnYao <76697563+ZhangEnYao@users.noreply.github.com>
2025-06-20 18:32:56 +0800

    feat(input): add Measurement input component and update types (#101)

--- web/src/lib/components/custom/input/single/index.ts
+++ web/src/lib/components/custom/input/single/index.ts
@@ -1,15 +1,14 @@
+import { default as Measurement } from './input-measurement.svelte';
 import { default as Boolean } from './input-boolean.svelte';
 import { default as Color } from './input-color.svelte';
 import { default as DeletionConfirm } from './input-deletion-confirm.svelte';
 import { default as General } from './input-general.svelte';
 import { default as Password } from './input-password.svelte';
-import type { InputType } from './types';
+import type { InputType, UnitType } from './types';
 import { PasswordManager, typeToIcon } from './utils.svelte';
 
-export {
-    Boolean, Color, DeletionConfirm, General, Password, PasswordManager, typeToIcon
-};
+export { Measurement, Boolean, Color, DeletionConfirm, General, Password, PasswordManager, typeToIcon };
 export type {
-    InputType
+    InputType, UnitType
 };
 
