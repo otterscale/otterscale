a5fffd1b41a763af537d1643062bcadd52feb30b
Zhang, En-Yao <enyao_chang@phison.com>
2025-07-11 10:35:55 +0800

    feat: implement layered architecture for storage management

--- web/src/lib/components/management-storage/index.ts
+++ web/src/lib/components/management-storage/index.ts
@@ -1,13 +1,13 @@
 
-import { Bucket } from './storage-bucket';
-import { Image } from './storage-image';
-import { Monitor } from './storage-monitor';
-import { ObjectStorageDaemon } from './storage-object-storage-daemon';
-import { Pool } from './storage-pool';
-import { Subvolume } from './storage-subvolume';
-import { SubvolumeGroup } from './storage-subvolume-group';
-import { User } from './storage-user';
-import { Volume } from './storage-volume';
+import { Bucket } from './bucket';
+import { Image } from './image';
+import { Monitor } from './monitor';
+import { ObjectStorageDaemon } from './object-storage-daemon';
+import { Pool } from './pool';
+import { Subvolume } from './subvolume';
+import { SubvolumeGroup } from './subvolume-group';
+import { User } from './user';
+import { Volume } from './volume';
 
 export { Bucket, Image, Monitor, ObjectStorageDaemon, Pool, Subvolume, SubvolumeGroup, User, Volume };
 
