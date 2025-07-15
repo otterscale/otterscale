ab5e80db3263c54132284a5e30871f63ebc48205
Paul Tsai <paul_tsai@phison.com>
2025-07-15 15:06:42 +0800

    feat: add account and settings paths to routing

--- otterscale-web/src/lib/path.ts
+++ otterscale-web/src/lib/path.ts
@@ -1,4 +1,6 @@
 export const loginPath = "/login";
 export const dashboardPath = "/dashboard";
+export const accountPath = "/account"
+export const settingsPath = "/settings"
 export const termsOfServicePath = "/terms-of-service";
 export const privacyPolicyPath = "/privacy-policy";
