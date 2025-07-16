94787a759634035444ea57388ef7956d20dc73ee
woody_lin <jj7158761@gmail.com>
2025-07-16 16:59:04 +0800

    refactor: rename delete and edit file

--- web/src/lib/components/bist/testing-tools/data-table/actions.svelte
+++ web/src/lib/components/bist/testing-tools/data-table/actions.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import Icon from '@iconify/svelte';
-
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-
-	import ActionEdit from './action-edit.svelte';
-	import ActionDelete from './action-delete.svelte';
+	import Icon from '@iconify/svelte';
+	import Delete from './delete.svelte';
+	import Edit from './edit.svelte';
 	import type { FlexibleIOTest } from './types';
 
 	let { flexibleIOTest }: { flexibleIOTest: FlexibleIOTest } = $props();
@@ -23,16 +21,11 @@
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content class="[&_.action]:cursor-pointer [&_.action]:text-xs [&_.label]:text-xs">
 		<DropdownMenu.Group>
-			<!-- <DropdownMenu.Label class="label">Views</DropdownMenu.Label>
 			<DropdownMenu.Item class="action" onSelect={(e) => e.preventDefault()}>
-				<ActionSubvolume />
-			</DropdownMenu.Item>
-			<DropdownMenu.Label class="label">Actions</DropdownMenu.Label> -->
-			<DropdownMenu.Item class="action" onSelect={(e) => e.preventDefault()}>
-				<ActionEdit {flexibleIOTest} />
+				<Edit {flexibleIOTest} />
 			</DropdownMenu.Item>
 			<DropdownMenu.Item class="action" onSelect={(e) => e.preventDefault()}>
-				<ActionDelete {flexibleIOTest} />
+				<Delete {flexibleIOTest} />
 			</DropdownMenu.Item>
 		</DropdownMenu.Group>
 	</DropdownMenu.Content>
