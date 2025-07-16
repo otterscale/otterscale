d478857665904f110ce761e5b2b621773f021d78
Paul Tsai <paul_tsai@phison.com>
2025-07-16 10:54:44 +0800

    feat: add sheet bookmark and notification components

---
+++ otterscale-web/src/lib/components/layout/sheet-bookmark.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import * as Sheet from '$lib/components/ui/sheet/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+
+	let { open = $bindable(false) }: { open: boolean } = $props();
+</script>
+
+<Sheet.Root bind:open>
+	<Sheet.Content side="right">
+		<Sheet.Header>
+			<Sheet.Title>Title</Sheet.Title>
+			<Sheet.Description>Description</Sheet.Description>
+		</Sheet.Header>
+		<Sheet.Footer>
+			<Sheet.Close class={buttonVariants({ variant: 'outline' })}>Save changes</Sheet.Close>
+		</Sheet.Footer>
+	</Sheet.Content>
+</Sheet.Root>
