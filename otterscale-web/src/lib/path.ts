7d7340cfd6fd80e686fd993c05e62370223b98e9
Paul Tsai <paul_tsai@phison.com>
2025-07-16 14:31:29 +0800

    feat: update locale entries in message files and reorganize path exports

--- otterscale-web/src/lib/path.ts
+++ otterscale-web/src/lib/path.ts
@@ -1,6 +1,8 @@
-export const loginPath = "/login";
-export const dashboardPath = "/dashboard";
 export const accountPath = "/account"
+export const dashboardPath = "/dashboard";
+export const documentationPath = "https://otterscale.github.io";
+export const feedbackPath = "https://github.com/otterscale/otterscale/issues/new/choose";
+export const loginPath = "/login";
+export const privacyPolicyPath = "/privacy-policy";
 export const settingsPath = "/settings"
 export const termsOfServicePath = "/terms-of-service";
-export const privacyPolicyPath = "/privacy-policy";
