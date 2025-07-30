204709acaefdd29f4a870545679323ff9dbc212f
Paul Tsai <paul_tsai@phison.com>
2025-07-30 18:33:36 +0800

    feat: add setup environment section with code snippet and copy button

--- otterscale-web/src/routes/(auth)/setup/+page.svelte
+++ otterscale-web/src/routes/(auth)/setup/+page.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import { homePath, setupPath } from '$lib/path';
 	import { breadcrumb } from '$lib/stores';
+	import * as Code from '$lib/components/custom/code';
+	import { m } from '$lib/paraglide/messages';
 
 	// Set breadcrumb navigation
 	breadcrumb.set({ parents: [homePath], current: setupPath });
+
+	const code = `sh -c "$(curl -fsSL https://raw.githubusercontent.com/otterscale/otterscale/main/scripts/install.sh" -- url=${PUBLIC_API_URL})`;
 </script>
 
-Setup Enviroment
+<div class="flex flex-col items-center">
+	<h2 class="text-center text-3xl font-bold tracking-tight sm:text-4xl">{m.setup_environment()}</h2>
+	<p class="text-muted-foreground mt-4 text-center text-lg">
+		{m.setup_environment_description()}
+	</p>
+	<div class="min-w-6xl p-6">
+		<Code.Root lang="bash" class="w-full" variant="secondary" {code} hideLines>
+			<Code.CopyButton />
+		</Code.Root>
+	</div>
+</div>
