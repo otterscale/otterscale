b8f366a8e13dfc73a08144e96a05597fa240b07e
Paul Tsai <paul_tsai@phison.com>
2025-08-05 15:09:59 +0800

    refactor: update animation class for HomeCard and adjust grid layout in +page component

--- otterscale-web/src/lib/components/scopes/home-card.svelte
+++ otterscale-web/src/lib/components/scopes/home-card.svelte
@@ -19,7 +19,7 @@
 			class="absolute inset-0 flex h-full items-center justify-center saturate-60 transition-opacity group-hover:opacity-80 dark:saturate-100 {background}"
 		>
 			<div
-				class="bg-primary/30 mx-auto flex size-18 overflow-hidden rounded-full shadow-md group-hover:animate-bounce"
+				class="bg-primary/30 mx-auto flex size-18 overflow-hidden rounded-full shadow-md group-hover:scale-105"
 			>
 				<Icon icon="{urlIcon(path.url)}-fill" class="text-muted m-auto size-8" />
 			</div>
