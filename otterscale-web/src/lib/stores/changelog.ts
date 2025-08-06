39a98304ae92119318d6710aefdff4f329fd4cb5
Paul Tsai <paul_tsai@phison.com>
2025-08-06 15:26:54 +0800

    feat: add changelog functionality and UI integration

---
+++ otterscale-web/src/lib/stores/changelog.ts
@@ -0,0 +1,13 @@
+import { browser } from "$app/environment";
+import { writable } from "svelte/store";
+
+const changelogVersionKey = 'changelogVersion'
+const changelogRead = writable(browser ? localStorage.getItem(changelogVersionKey) === import.meta.env.PACKAGE_VERSION : false)
+
+changelogRead.subscribe((value) => {
+    if (browser && value) {
+        localStorage.setItem(changelogVersionKey, import.meta.env.PACKAGE_VERSION)
+    }
+})
+
+export default changelogRead;
