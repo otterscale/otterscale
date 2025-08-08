3602095f489a5a0a66ccb85aac8baf9a4b0b0995
Paul Tsai <paul_tsai@phison.com>
2025-08-08 10:35:41 +0800

    feat: add Single Sign-On (SSO) configuration and UI for OIDC provider setup

--- otterscale-web/src/lib/auth-client.ts
+++ otterscale-web/src/lib/auth-client.ts
@@ -1,6 +1,10 @@
-import { env } from "$env/dynamic/public";
 import { createAuthClient } from "better-auth/svelte";
+import { ssoClient } from "@better-auth/sso/client"
+import { env } from "$env/dynamic/public";
 
 export const authClient = createAuthClient({
     baseURL: env.PUBLIC_URL,
+    plugins: [
+        ssoClient()
+    ]
 });
