5fa76b5a4089ab24ca946014ce7bcb9be5c3d5bb
terrywu510 <terrywu25@gmail.com>
2025-08-11 17:52:18 +0800

    update kubevirt

--- internal/app/kubevirt.go
+++ internal/app/kubevirt.go
@@ -6,7 +6,6 @@ import (
 	"connectrpc.com/connect"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
-	v1 "k8s.io/api/core/v1"
 
 	pb "github.com/openhdc/otterscale/api/kubevirt/v1"
 	"github.com/openhdc/otterscale/api/kubevirt/v1/pbconnect"
@@ -427,39 +426,7 @@ func toProtoDataVolume(dv *core.DataVolume) *pb.DataVolume {
 		Annotations: dv.GetAnnotations(),
 		CreatedAt:   timestamppb.New(dv.CreationTimestamp.Time),
 	}))
-	var source string
-	var sourceType string
-	var sizeBytes int64
-
-	if dv.Spec.PVC != nil {
-		if dv.Spec.PVC.Resources.Requests != nil {
-			size, found := dv.Spec.PVC.Resources.Requests[v1.ResourceStorage]
-			if found {
-				sizeBytes = size.Value()
-			}
-		}
-	} else if dv.Spec.Storage != nil {
-		if dv.Spec.Storage.Resources.Requests != nil {
-			size, found := dv.Spec.Storage.Resources.Requests[v1.ResourceStorage]
-			if found {
-				sizeBytes = size.Value()
-			}
-		}
-	}
-	if dv.Spec.Source.HTTP != nil {
-		source = dv.Spec.Source.HTTP.URL
-		sourceType = "HTTP"
-	} else if dv.Spec.Source.Upload != nil {
-		source = ""
-		sourceType = "Upload"
-	} else if dv.Spec.Source.S3 != nil {
-		source = dv.Spec.Source.S3.URL
-		sourceType = "S3"
-	} else if dv.Spec.Source.VDDK != nil {
-		source = dv.Spec.Source.VDDK.URL
-		sourceType = string(dv.Spec.Source.VDDK.UUID)
-	}
-
+	source, sourceType, sizeBytes := core.ExtractDataVolumeInfo(dv)
 	ret.SetType(sourceType)
 	ret.SetSizeBytes(sizeBytes)
 	ret.SetSource(source)
