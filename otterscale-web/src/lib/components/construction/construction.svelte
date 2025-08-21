9e8812531a3feef41628eca416e99c8cf3d1cd4e
Paul Tsai <paul_tsai@phison.com>
2025-08-21 11:03:53 +0800

    fix: adjust icon size and change icon for construction component

--- otterscale-web/src/lib/components/construction/construction.svelte
+++ otterscale-web/src/lib/components/construction/construction.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="pointer-events-none fixed inset-0 flex flex-col items-center justify-center gap-2">
-	<div class="border-border flex size-28 items-center justify-center rounded-full border-2">
-		<Icon icon="ph:rocket-launch" class="size-16" />
+	<div class="border-border flex size-24 items-center justify-center rounded-full border-2">
+		<Icon icon="ph:magic-wand" class="size-14" />
 	</div>
 
 	<p class="pt-2 text-3xl">{m.under_development()}</p>
