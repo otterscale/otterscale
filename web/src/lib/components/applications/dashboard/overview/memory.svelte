2271ce0721b2fd4aa54feb39d5376af93b449801
Zhang, En-Yao <enyao_chang@phison.com>
2025-09-01 19:28:04 +0800

    refactor: applications overview dashboard

---
+++ web/src/lib/components/applications/dashboard/overview/memory.svelte
@@ -0,0 +1,182 @@
+<script lang="ts">
+	import { ReloadManager } from '$lib/components/custom/reloader';
+	import * as Card from '$lib/components/ui/card';
+	import * as Chart from '$lib/components/ui/chart/index.js';
+	import { formatCapacity } from '$lib/formatter';
+	import { m } from '$lib/paraglide/messages';
+	import { currentKubernetes } from '$lib/stores';
+	import { cn } from '$lib/utils';
+	import { scaleUtc } from 'd3-scale';
+	import { curveNatural } from 'd3-shape';
+	import { Area, AreaChart, LinearGradient } from 'layerchart';
+	import { PrometheusDriver, SampleValue } from 'prometheus-query';
+	import { onMount } from 'svelte';
+
+	let {
+		prometheusDriver,
+		isReloading = $bindable(),
+		span
+	}: { prometheusDriver: PrometheusDriver; isReloading: boolean; span: string } = $props();
+
+	let memoryUsages: SampleValue[] = $state([]);
+	const memoryUsagesConfiguration = {
+		usage: { label: 'Usage', color: 'var(--chart-1)' }
+	} satisfies Chart.ChartConfig;
+	let memoryRequests = $state(0);
+	let memoryLimits = $state(0);
+
+	function fetch() {
+		prometheusDriver
+			.rangeQuery(
+				`
+						sum(
+						container_memory_rss{container!="",job="kubelet",juju_model_uuid="${$currentKubernetes?.scopeUuid}",metrics_path="/metrics/cadvisor"}
+						)
+						`,
+				Date.now() - 60 * 60 * 1000,
+				Date.now(),
+				2 * 60
+			)
+			.then((response) => {
+				memoryUsages = response.result[0].values;
+			});
+		prometheusDriver
+			.instantQuery(
+				`
+						sum(
+							namespace_memory:kube_pod_container_resource_requests:sum{juju_model_uuid="${$currentKubernetes?.scopeUuid}"}
+						)
+						/
+						sum(
+							kube_node_status_allocatable{job="kube-state-metrics",juju_model_uuid="${$currentKubernetes?.scopeUuid}",resource="memory"}
+						)
+						`
+			)
+			.then((response) => {
+				memoryRequests = response.result[0].value.value;
+			});
+		prometheusDriver
+			.instantQuery(
+				`
+						sum(
+							namespace_memory:kube_pod_container_resource_limits:sum{juju_model_uuid="${$currentKubernetes?.scopeUuid}"}
+						)
+						/
+						sum(
+							kube_node_status_allocatable{job="kube-state-metrics",juju_model_uuid="${$currentKubernetes?.scopeUuid}",resource="memory"}
+						)
+						`
+			)
+			.then((response) => {
+				memoryLimits = response.result[0].value.value;
+			});
+	}
+
+	const reloadManager = new ReloadManager(fetch);
+
+	let isLoading = $state(true);
+	onMount(async () => {
+		await fetch();
+		isLoading = false;
+	});
+
+	$effect(() => {
+		if (isReloading) {
+			reloadManager.restart();
+		} else {
+			reloadManager.stop();
+		}
+	});
+</script>
+
+{#if isLoading}
+	Loading
+{:else}
+	<Card.Root class={cn('gap-2', span)}>
+		<Card.Header>
+			<Card.Title>{m.memory_usage()}</Card.Title>
+			<Card.Action class="text-muted-foreground flex flex-col gap-0.5 text-sm">
+				<div class="flex justify-between gap-2">
+					<p>{m.requests()}</p>
+					<p class="font-mono">{Math.round(memoryRequests * 100)}%</p>
+				</div>
+				<div class="flex justify-between gap-2">
+					<p>{m.limits()}</p>
+					<p class="font-mono">{Math.round(memoryLimits * 100)}%</p>
+				</div>
+			</Card.Action>
+		</Card.Header>
+		<Card.Content>
+			<Chart.Container config={memoryUsagesConfiguration}>
+				<AreaChart
+					data={memoryUsages}
+					x="time"
+					xScale={scaleUtc()}
+					yPadding={[0, 25]}
+					series={[
+						{
+							key: 'value',
+							label: memoryUsagesConfiguration.usage.label,
+							color: memoryUsagesConfiguration.usage.color
+						}
+					]}
+					seriesLayout="stack"
+					props={{
+						area: {
+							curve: curveNatural,
+							'fill-opacity': 0.4,
+							line: { class: 'stroke-1' },
+							motion: 'tween'
+						},
+						xAxis: {
+							format: (v: Date) =>
+								`${v.getHours().toString().padStart(2, '0')}:${v.getMinutes().toString().padStart(2, '0')}`
+						},
+						yAxis: { format: () => '' }
+					}}
+				>
+					{#snippet tooltip()}
+						<Chart.Tooltip
+							indicator="dot"
+							labelFormatter={(v: Date) => {
+								return v.toLocaleDateString('en-US', {
+									year: 'numeric',
+									month: 'short',
+									day: 'numeric',
+									hour: 'numeric',
+									minute: 'numeric'
+								});
+							}}
+						>
+							{#snippet formatter({ item, name, value })}
+								{@const { value: capacity, unit } = formatCapacity(Number(value))}
+								<div
+									style="--color-bg: {item.color}"
+									class="aspect-square h-full w-fit shrink-0 border-(--color-border) bg-(--color-bg)"
+								></div>
+								<div class="flex flex-1 shrink-0 items-center justify-between text-xs leading-none">
+									<div class="grid gap-1.5">
+										<span class="text-muted-foreground">{name}</span>
+									</div>
+									<p class="font-mono">{capacity} {unit}</p>
+								</div>
+							{/snippet}
+						</Chart.Tooltip>
+					{/snippet}
+					{#snippet marks({ series, getAreaProps })}
+						{#each series as s, i (s.key)}
+							<LinearGradient
+								stops={[s.color ?? '', 'color-mix(in lch, ' + s.color + ' 10%, transparent)']}
+								vertical
+							>
+								{#snippet children({ gradient })}
+									<Area {...getAreaProps(s, i)} fill={gradient} />
+								{/snippet}
+							</LinearGradient>
+						{/each}
+					{/snippet}
+				</AreaChart>
+			</Chart.Container>
+		</Card.Content>
+	</Card.Root>
+{/if}
