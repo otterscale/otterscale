f5cdfa764d94b83a1a25f085d059ea1dcc4014a3
Zhang, En-Yao <enyao_chang@phison.com>
2025-09-05 19:59:45 +0800

    feat: expand the detail tooltip trigger area of progress

--- web/src/lib/components/custom/progress/progress.svelte
+++ web/src/lib/components/custom/progress/progress.svelte
@@ -32,32 +32,36 @@
 </script>
 
 {#if denominator > 0}
-	<div bind:this={ref} data-slot="progress-root" {...restProps}>
-		<Progress value={progressRatio} max={1} class={formatProgressColor(progressRatio, false, highIsGood)} />
-		{#if ratio}
-			<div
-				class={cn(
-					'text-muted-foreground flex items-center justify-end font-light sm:min-w-[100px] md:min-w-[200px]',
-					className,
-				)}
-			>
-				{#if detail}
-					<Tooltip.Provider>
-						<Tooltip.Root>
-							<Tooltip.Trigger>
-								{@render ratio?.({ numerator, denominator })}
-							</Tooltip.Trigger>
-							<Tooltip.Content>
+	<Tooltip.Provider>
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<div bind:this={ref} data-slot="progress-root" {...restProps}>
+					<Progress
+						value={progressRatio}
+						max={1}
+						class={formatProgressColor(progressRatio, false, highIsGood)}
+					/>
+					{#if ratio}
+						<div
+							class={cn(
+								'text-muted-foreground flex items-center justify-end font-light sm:min-w-[100px] md:min-w-[200px]',
+								className,
+							)}
+						>
+							{#if detail}
 								{@render detail?.({ numerator, denominator })}
-							</Tooltip.Content>
-						</Tooltip.Root>
-					</Tooltip.Provider>
-				{:else}
-					{@render ratio?.({ numerator, denominator })}
-				{/if}
-			</div>
-		{/if}
-	</div>
+							{:else}
+								{@render ratio?.({ numerator, denominator })}
+							{/if}
+						</div>
+					{/if}
+				</div>
+			</Tooltip.Trigger>
+			<Tooltip.Content>
+				{@render detail?.({ numerator, denominator })}
+			</Tooltip.Content>
+		</Tooltip.Root>
+	</Tooltip.Provider>
 {:else}
 	<Icon icon="ph:infinity" />
 {/if}
