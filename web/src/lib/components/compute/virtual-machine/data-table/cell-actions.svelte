e2f68655a0e4db477d858ee5575eb404e6260d30
woody_lin <jj7158761@gmail.com>
2025-09-10 21:57:56 +0800

    feat: add virtual machine control actions (start, stop, pause, resume, clone) and localization support

--- web/src/lib/components/compute/virtual-machine/data-table/cell-actions.svelte
+++ web/src/lib/components/compute/virtual-machine/data-table/cell-actions.svelte
@@ -1,5 +1,8 @@
 <script lang="ts" module>
+	import Clone from './action-clone.svelte';
+	import PauseResume from './action-pause-resume.svelte';
 	import Snapshot from './action-snapshot.svelte';
+	import StartStop from './action-start-stop.svelte';
 	import StartupScript from './action-startup-script.svelte';
 
 	import type { VirtualMachine } from '$lib/api/kubevirt/v1/kubevirt_pb';
@@ -13,6 +16,15 @@
 
 <Layout.Actions>
 	<Layout.ActionLabel>{m.actions()}</Layout.ActionLabel>
+	<Layout.ActionItem>
+		<PauseResume {virtualMachine} />
+	</Layout.ActionItem>
+	<Layout.ActionItem>
+		<StartStop {virtualMachine} />
+	</Layout.ActionItem>
+	<Layout.ActionItem>
+		<Clone {virtualMachine} />
+	</Layout.ActionItem>
 	<Layout.ActionItem>
 		<Snapshot {virtualMachine} />
 	</Layout.ActionItem>
