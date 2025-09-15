e63019c46ee78d81c9b38cab926a0dcd47b84b8b
woody_lin <jj7158761@gmail.com>
2025-09-15 14:07:41 +0800

    feat: add delete virtual machine functionality and update UI components

--- web/src/lib/components/compute/virtual-machine/data-table/cell-actions.svelte
+++ web/src/lib/components/compute/virtual-machine/data-table/cell-actions.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" module>
 	import Clone from './action-clone.svelte';
+	import Delete from './action-delete.svelte';
 	import PauseResume from './action-pause-resume.svelte';
 	import Snapshot from './action-snapshot.svelte';
 	import StartStop from './action-start-stop.svelte';
@@ -23,12 +24,15 @@
 		<StartStop {virtualMachine} />
 	</Layout.ActionItem>
 	<Layout.ActionItem>
-		<Clone {virtualMachine} />
+		<StartupScript {virtualMachine} />
 	</Layout.ActionItem>
 	<Layout.ActionItem>
 		<Snapshot {virtualMachine} />
 	</Layout.ActionItem>
 	<Layout.ActionItem>
-		<StartupScript {virtualMachine} />
+		<Clone {virtualMachine} />
+	</Layout.ActionItem>
+	<Layout.ActionItem>
+		<Delete {virtualMachine} />
 	</Layout.ActionItem>
 </Layout.Actions>
