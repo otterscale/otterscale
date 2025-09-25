72d09563ea89c44233ec534ad4ec54e8fbd8e17d
Zhang, En-Yao <enyao_chang@phison.com>
2025-09-25 17:39:02 +0800

    feat: update triggerVariants base style and disable delete button for built-in tags

--- web/src/lib/components/custom/modal/single-step/utils.ts
+++ web/src/lib/components/custom/modal/single-step/utils.ts
@@ -4,7 +4,7 @@ import { buttonVariants } from '$lib/components/ui/button';
 import { cn } from '$lib/utils';
 
 const triggerVariants = tv({
-	base: 'disabled:text-muted-foreground flex h-full w-full items-center gap-1 disabled:pointer-events-auto disabled:cursor-not-allowed',
+	base: 'disabled:text-muted-foreground disabled:**:text-muted-foreground flex h-full w-full items-center gap-1 disabled:pointer-events-auto disabled:cursor-not-allowed',
 	variants: {
 		variant: {
 			default: cn(buttonVariants({ variant: 'ghost', size: 'default' }), 'w-fit'),
