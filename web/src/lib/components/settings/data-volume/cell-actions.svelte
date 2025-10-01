d0ff45b3dade03ad97ce756c863e45f917112b51
woody_lin <jj7158761@gmail.com>
2025-10-01 18:41:28 +0800

    feat: remove unused action components from data volume cell actions

--- web/src/lib/components/settings/data-volume/cell-actions.svelte
+++ web/src/lib/components/settings/data-volume/cell-actions.svelte
@@ -1,12 +1,6 @@
 <script lang="ts" module>
 	import Delete from './action-delete.svelte';
 	import Extend from './action-extend.svelte';
-	// import Migrate from './action-migrate.svelte';
-	// import PauseResume from './action-pause-resume.svelte';
-	// import Restart from './action-restart.svelte';
-	// import Restore from './action-restore.svelte';
-	// import Snapshot from './action-snapshot.svelte';
-	// import StartStop from './action-start-stop.svelte';
 
 	import type { DataVolume } from '$lib/api/virtual_machine/v1/virtual_machine_pb';
 	import * as Layout from '$lib/components/custom/data-table/layout';
@@ -26,28 +20,4 @@
 	<Layout.ActionItem>
 		<Delete {dataVolume} />
 	</Layout.ActionItem>
-	<!-- <Layout.ActionItem>
-		<PauseResume {virtualMachine} />
-	</Layout.ActionItem>
-	<Layout.ActionItem>
-		<StartStop {virtualMachine} />
-	</Layout.ActionItem>
-	<Layout.ActionItem>
-		<Restart {virtualMachine} />
-	</Layout.ActionItem>
-	<Layout.ActionSeparator />
-	<Layout.ActionItem>
-		<Snapshot {virtualMachine} />
-	</Layout.ActionItem>
-	<Layout.ActionItem>
-		<Restore {virtualMachine} />
-	</Layout.ActionItem>
-	<Layout.ActionSeparator />
-	<Layout.ActionItem>
-		<Clone {virtualMachine} />
-	</Layout.ActionItem>
-	<Layout.ActionItem>
-		<Migrate {virtualMachine} />
-	</Layout.ActionItem>
- -->
 </Layout.Actions>
