23514d1a00074576fa735a3ccb81cf42eb2cdf64
woody_lin <jj7158761@gmail.com>
2025-10-02 14:41:57 +0800

    feat: add application scaling and restart actions to data table

---
+++ web/src/lib/components/applications/workloads/data-table/action-restart.svelte
@@ -0,0 +1,67 @@
+<script lang="ts" module>
+	import { ConnectError, createClient, type Transport } from '@connectrpc/connect';
+	import Icon from '@iconify/svelte';
+	import { getContext } from 'svelte';
+	import { toast } from 'svelte-sonner';
+
+	import { ApplicationService, type Application } from '$lib/api/application/v1/application_pb';
+	import { m } from '$lib/paraglide/messages';
+	import { currentKubernetes } from '$lib/stores';
+</script>
+
+<script lang="ts">
+	let { application }: { application: Application } = $props();
+
+	const transport: Transport = getContext('transport');
+	const applicationClient = createClient(ApplicationService, transport);
+	let loading = $state(false);
+
+	async function restartApplication() {
+		const request = {
+			scopeUuid: $currentKubernetes?.scopeUuid,
+			facilityName: $currentKubernetes?.name,
+			name: application.name,
+			namespace: application.namespace,
+			type: application.type,
+		};
+
+		toast.promise(() => applicationClient.restartApplication(request), {
+			loading: `Restarting application ${request.name}...`,
+			success: () => `Successfully restarted application ${request.name}.`,
+			error: (e) => {
+				const msg = `Failed to restart application ${request.name}.`;
+				toast.error(msg, {
+					description: (e as ConnectError).message.toString(),
+					duration: Number.POSITIVE_INFINITY,
+				});
+				return msg;
+			},
+		});
+	}
+
+	async function handleClick() {
+		loading = true;
+
+		try {
+			await restartApplication();
+		} finally {
+			loading = false;
+		}
+	}
+</script>
+
+<div class="flex items-center justify-end gap-1">
+	<button
+		onclick={handleClick}
+		disabled={loading}
+		class="flex items-center gap-1 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+	>
+		{#if loading}
+			<Icon icon="ph:spinner-gap" class="animate-spin" />
+			{m.please_wait()}
+		{:else}
+			<Icon icon="ph:arrow-clockwise" class="text-accent-foreground" />
+			{m.restarts()}
+		{/if}
+	</button>
+</div>
