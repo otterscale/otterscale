e94c15767180ffd4eec9b9352fa466194170f502
Paul Tsai <paul_tsai@phison.com>
2025-10-14 06:15:46 +0000

    feat: implement bootstrap and serve commands with HTTP server support

--- internal/cmd/serve.go
+++ internal/cmd/serve.go
@@ -2,24 +2,12 @@ package cmd
 
 import (
 	"log/slog"
-	"net"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/rs/cors"
 	"github.com/spf13/cobra"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 
 	"github.com/otterscale/otterscale/internal/config"
 	"github.com/otterscale/otterscale/internal/mux"
-)
-
-const (
-	containerEnvVar            = "OTTERSCALE_CONTAINER"
-	defaultContainerAddress    = ":8299"
-	defaultContainerConfigPath = "/etc/app/otterscale.yaml"
 )
 
 func NewServe(conf *config.Config, serve *mux.Serve) *cobra.Command {
@@ -27,11 +15,10 @@ func NewServe(conf *config.Config, serve *mux.Serve) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:     "serve",
-		Short:   "Start the OtterScale API server",
-		Long:    "Start the OtterScale API server that provides gRPC and HTTP endpoints for all services",
+		Short:   "Start the main API server",
+		Long:    "Start the OtterScale API server that provides gRPC and HTTP endpoints for the core services (used after bootstrap)",
 		Example: "otterscale serve --address=:8299 --config=otterscale.yaml",
 		RunE: func(_ *cobra.Command, _ []string) error {
-			// Check if running in container and override address
 			if os.Getenv(containerEnvVar) != "" {
 				address = defaultContainerAddress
 				configPath = defaultContainerConfigPath
@@ -43,25 +30,7 @@ func NewServe(conf *config.Config, serve *mux.Serve) *cobra.Command {
 				return err
 			}
 
-			srv := &http.Server{
-				Addr: address,
-				Handler: h2c.NewHandler(
-					cors.AllowAll().Handler(serve),
-					&http2.Server{},
-				),
-				ReadHeaderTimeout: time.Second,
-				ReadTimeout:       5 * time.Minute,
-				WriteTimeout:      5 * time.Minute,
-				MaxHeaderBytes:    8 * 1024, // 8KiB
-			}
-
-			listener, err := net.Listen("tcp", address) //nolint:noctx // context not needed for Listen
-			if err != nil {
-				return err
-			}
-
-			slog.Info("Server starting on", "address", listener.Addr().String())
-			return srv.Serve(listener)
+			return startHTTPServer(address, serve)
 		},
 	}
 
