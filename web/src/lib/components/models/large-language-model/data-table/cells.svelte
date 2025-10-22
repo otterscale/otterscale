f03190f901d6f08b7d47a00debb9058bf39f9da4
Woody Lin <jj7158761@gmail.com>
2025-10-22 12:24:28 +0000

    fix: adjust pagination sizes across various data tables for consistency

--- web/src/lib/components/models/large-language-model/data-table/cells.svelte
+++ web/src/lib/components/models/large-language-model/data-table/cells.svelte
@@ -35,15 +35,13 @@
 {/snippet}
 
 {#snippet model(row: Row<LargeLanguageModel>)}
-	<Layout.Cell
-		class="items-start underline hover:cursor-pointer hover:no-underline"
-		onclick={() => {
-			goto(
-				`${dynamicPaths.applicationsWorkloads(page.params.scope).url}/${row.original.application.namespace}/${row.original.application.name}`,
-			);
-		}}
-	>
-		{row.original.application.name}
+	<Layout.Cell class="items-start">
+		<a
+			class="m-0 p-0 underline hover:no-underline"
+			href={`${dynamicPaths.applicationsWorkloads(page.params.scope).url}/${row.original.application.namespace}/${row.original.application.name}`}
+		>
+			{row.original.application.name}
+		</a>
 		<Layout.SubCell>
 			{row.original.application.namespace}
 		</Layout.SubCell>
