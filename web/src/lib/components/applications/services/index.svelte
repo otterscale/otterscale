4c158ad70d9b56bdc32dc0f2514212995b1da529
ZhangEnYao <76697563+ZhangEnYao@users.noreply.github.com>
2025-10-30 17:45:37 +0800

    feat: services management (#798)
    * feat: add services data table and related components for application management
    
    * feat: refactor application services data table components and introduce Service type
    
    * feat: add error handling for data loading and update modelName to use dynamic value
    
    ---------
    
    Co-authored-by: Paul Tsai <paul_tsai@phison.com>

---
+++ web/src/lib/components/applications/services/index.svelte
@@ -0,0 +1,79 @@
+<script lang="ts" module>
+	import { createClient, type Transport } from '@connectrpc/connect';
+	import { getContext, onDestroy, onMount, setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	import { DataTable } from './data-table/index';
+	import type { Service } from './types';
+
+	import { ApplicationService } from '$lib/api/application/v1/application_pb';
+	import * as Loading from '$lib/components/custom/loading';
+	import { ReloadManager } from '$lib/components/custom/reloader';
+</script>
+
+<script lang="ts">
+	let { scope, facility }: { scope: string; facility: string } = $props();
+
+	const transport: Transport = getContext('transport');
+	let isMounted = $state(false);
+
+	const services = writable<Service[]>([]);
+
+	const applicationClient = createClient(ApplicationService, transport);
+	const reloadManager = new ReloadManager(() => {
+		applicationClient
+			.listApplications({
+				scope: scope,
+				facility: facility,
+			})
+			.then((response) => {
+				services.set(
+					response.applications.flatMap((application) =>
+						application.services.map((service) => ({
+							...service,
+							publicAddress: application.publicAddress,
+						})),
+					),
+				);
+			})
+			.catch((error) => {
+				console.error('Error during data loading:', error);
+			});
+	});
+	setContext('reloadManager', reloadManager);
+
+	onMount(() => {
+		applicationClient
+			.listApplications({
+				scope: scope,
+				facility: facility,
+			})
+			.then((response) => {
+				services.set(
+					response.applications.flatMap((application) =>
+						application.services.map((service) => ({
+							...service,
+							publicAddress: application.publicAddress,
+						})),
+					),
+				);
+				isMounted = true;
+			})
+			.catch((error) => {
+				console.error('Error during initial data load:', error);
+			});
+
+		reloadManager.start();
+	});
+	onDestroy(() => {
+		reloadManager.stop();
+	});
+</script>
+
+<main class="space-y-4 py-4">
+	{#if isMounted}
+		<DataTable {services} {reloadManager} />
+	{:else}
+		<Loading.DataTable />
+	{/if}
+</main>
---
+++ web/src/lib/components/applications/services/index.ts
@@ -0,0 +1,3 @@
+import Services from './index.svelte';
+
+export { Services };
