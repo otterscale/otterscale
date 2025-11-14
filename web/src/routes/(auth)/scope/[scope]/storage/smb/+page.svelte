3bf1aaeb072c9b39e7e172ac059e9698c8a82d65
ZhangEnYao <76697563+ZhangEnYao@users.noreply.github.com>
2025-11-14 18:06:22 +0800

    874 implement smb share management (#883)
    * feat(storage): implement SMB share management UI
    
    * feat(storage): enhance SMB share management with user creation and import functionality, add tag group component for better user display
    
    * feat(storage): add detailed descriptions for SMB share settings and improve user prompts
    
    ---------
    
    Co-authored-by: Zhang, En-Yao <enyaochang@phison.com>

---
+++ web/src/routes/(auth)/scope/[scope]/storage/smb/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" module>
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import { SMB } from '$lib/components/storage/smb/index';
+	import { m } from '$lib/paraglide/messages';
+	import { breadcrumbs } from '$lib/stores';
+</script>
+
+<script lang="ts">
+	breadcrumbs.set([
+		{
+			title: m.storage(),
+			url: resolve('/(auth)/scope/[scope]/storage', { scope: page.params.scope! })
+		},
+		{
+			title: m.file_system(),
+			url: resolve('/(auth)/scope/[scope]/storage/file-system', { scope: page.params.scope! })
+		}
+	]);
+</script>
+
+<SMB scope={page.params.scope!} facility="bbb-kubernetes-control-plane" namespace="samba" />
