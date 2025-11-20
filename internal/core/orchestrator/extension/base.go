2a360d623c61f7a1ffdf0511456b4e0208383798
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-20 19:29:36 +0800

    feat: add CustomResourceDefinition repository implementation (#951)
    * feat: add CustomResourceDefinition repository implementation
    
    * fix: update k8s dependencies to latest versions in go.mod and go.sum
    
    * feat: refactor extension handling to support multiple charts per base
    
    * fix: update GPU Operator details and repository URL in base manifest
    
    * fix: update logo URLs for Gateway API and Samba operator in base manifest
    
    * fix: improve error handling and refactor CRD variable usage in Install and Upgrade extensions
    
    * feat: add extClientset method for external clientset retrieval
    
    * fix: correct Istio description formatting and update buildExtensionFromCRD function signature

--- internal/core/orchestrator/extension/base.go
+++ internal/core/orchestrator/extension/base.go
@@ -8,75 +8,170 @@ import (
 
 const chartRepoURL = "https://otterscale.github.io/charts"
 
-type base struct {
-	Name        string
+type chartManifest struct {
+	ID          string // If a value is present, it indicates a child chart.
 	Namespace   string
-	Version     string
 	RepoURL     string
 	Labels      map[string]string
 	Annotations map[string]string
 	ValuesMap   map[string]string
 }
 
+type crdManifest struct {
+	Version              string
+	RepoURL              string
+	AnnotationVersionKey string
+}
+
+type base struct {
+	ID          string
+	Name        string
+	Description string
+	Logo        string
+	Charts      []chartManifest
+	CRD         *crdManifest
+}
+
 var (
 	general = []base{
 		{
-			Name:      "kube-prometheus-stack",
-			Namespace: "monitoring",
-			RepoURL:   "https://prometheus-community.github.io/helm-charts",
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "gateway-api",
+			Name:        "Gateway API",
+			Description: "Gateway API is an official Kubernetes project focused on L4 and L7 routing in Kubernetes.",
+			Logo:        "https://github.com/kubernetes-sigs.png",
+			CRD: &crdManifest{
+				Version:              "v1.3.0",
+				RepoURL:              "https://github.com/kubernetes-sigs/gateway-api.git/config/crd",
+				AnnotationVersionKey: "gateway.networking.k8s.io/bundle-version",
+			},
+		},
+		{
+			ID:          "gateway-api-inference-extension",
+			Name:        "Gateway API Inference Extension",
+			Description: "Gateway API Inference Extension is an official Kubernetes project that optimizes self-hosting Generative Models on Kubernetes.",
+			Logo:        "https://github.com/kubernetes-sigs.png",
+			CRD: &crdManifest{
+				Version:              "v1.1.0",
+				RepoURL:              "https://github.com/kubernetes-sigs/gateway-api-inference-extension.git/config/crd",
+				AnnotationVersionKey: "inference.networking.k8s.io/bundle-version",
+			},
+		},
+		{
+			ID:          "base",
+			Name:        "Istio",
+			Description: "Istio is an open source service mesh that layers transparently onto existing distributed applications.",
+			Logo:        "https://github.com/istio.png",
+			Charts: []chartManifest{
+				{
+					Namespace: "istio-system",
+					RepoURL:   "https://istio-release.storage.googleapis.com/charts",
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+					},
+				},
+				{
+					ID:        "istiod",
+					Namespace: "istio-system",
+					RepoURL:   "https://istio-release.storage.googleapis.com/charts",
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+						"env.ENABLE_GATEWAY_API_INFERENCE_EXTENSION": "true",
+					},
+				},
+			},
+		},
+		{
+			ID:          "kube-prometheus-stack",
+			Name:        "Prometheus Stack",
+			Description: "Installs the kube-prometheus stack for easy, end-to-end Kubernetes cluster monitoring using the Prometheus Operator.",
+			Logo:        "https://github.com/prometheus-community.png",
+			Charts: []chartManifest{
+				{
+					Namespace: "monitoring",
+					RepoURL:   "https://prometheus-community.github.io/helm-charts",
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+					},
+				},
 			},
 		},
 	}
 
 	model = []base{
 		{
-			Name:      "gpu-operator",
-			Namespace: "nvidia-gpu-operator",
-			RepoURL:   "https://nvidia.github.io/gpu-operator",
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "gpu-operator",
+			Name:        "GPU Operator",
+			Description: "GPU Operator creates, configures, and manages GPUs in Kubernetes.",
+			Logo:        "https://github.com/otterscale.png",
+			Charts: []chartManifest{
+				{
+					Namespace: "gpu-operator",
+					RepoURL:   chartRepoURL,
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+					},
+				},
 			},
 		},
 		{
-			Name:      "llm-d-infra",
-			Namespace: "llm-d",
-			RepoURL:   "https://llm-d-incubation.github.io/llm-d-infra",
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
-				"nameOverride":    "llm-d-infra",
-				"gateway.gatewayParameters.resources.limits.cpu":    "4",
-				"gateway.gatewayParameters.resources.limits.memory": "2Gi",
-				"gateway.service.type":                              "NodePort",
+			ID:          "llm-d-infra",
+			Name:        "llm-d",
+			Description: "Achieve state of the art inference performance with modern accelerators on Kubernetes.",
+			Logo:        "https://github.com/llm-d.png",
+			Charts: []chartManifest{
+				{
+					Namespace: "llm-d",
+					RepoURL:   "https://llm-d-incubation.github.io/llm-d-infra",
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+						"nameOverride":    "llm-d-infra",
+						"gateway.gatewayParameters.resources.limits.cpu":    "4",
+						"gateway.gatewayParameters.resources.limits.memory": "2Gi",
+						"gateway.service.type":                              "NodePort",
+					},
+				},
 			},
 		},
 	}
 
 	instance = []base{
 		{
-			Name:      "kubevirt-infra",
-			Namespace: "kubevirt",
-			RepoURL:   chartRepoURL,
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "kubevirt-infra",
+			Name:        "KubeVirt",
+			Description: "Kubernetes Virtualization API and runtime in order to define and manage virtual machines.",
+			Logo:        "https://github.com/kubevirt.png",
+			Charts: []chartManifest{
+				{
+					Namespace: "kubevirt",
+					RepoURL:   chartRepoURL,
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+					},
+				},
 			},
 		},
 	}
 
 	storage = []base{
 		{
-			Name:      "samba-operator",
-			Namespace: "samba-operator",
-			RepoURL:   chartRepoURL,
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "samba-operator",
+			Name:        "Samba",
+			Description: "An operator for Samba as a service on PVCs in Kubernetes.",
+			Logo:        "https://github.com/otterscale.png",
+			Charts: []chartManifest{
+				{
+					Namespace: "samba-operator",
+					RepoURL:   chartRepoURL,
+					ValuesMap: map[string]string{
+						release.TypeLabel: "extension",
+					},
+				},
 			},
 		},
 	}
 )
 
-func (uc *UseCase) base(name string) (base, error) {
+func (uc *UseCase) base(id string) (base, error) {
 	all := []base{}
 	all = append(all, general...)
 	all = append(all, model...)
@@ -84,10 +179,10 @@ func (uc *UseCase) base(name string) (base, error) {
 	all = append(all, storage...)
 
 	for _, p := range all {
-		if p.Name == name {
+		if p.ID == id {
 			return p, nil
 		}
 	}
 
-	return base{}, fmt.Errorf("extension %s not found", name)
+	return base{}, fmt.Errorf("extension %s not found", id)
 }
