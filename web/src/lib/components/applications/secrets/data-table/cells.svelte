9be9bfdd7cb3df1ba7f639f19794a4494f726d7e
ZhangEnYao <76697563+ZhangEnYao@users.noreply.github.com>
2025-11-20 20:35:03 +0800

    feat: display secret data in a dialog with copy functionality
    * feat: display secret data in a dialog with copy functionality
    
    * feat: refactor dialog structure for secret name display
    
    * feat: update extension display to use correct name and description fields
    
    * feat: fix name

--- web/src/lib/components/applications/secrets/data-table/cells.svelte
+++ web/src/lib/components/applications/secrets/data-table/cells.svelte
@@ -3,15 +3,16 @@
 	import Icon from '@iconify/svelte';
 	import type { Row } from '@tanstack/table-core';
 
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
 	import type { Secret } from '$lib/api/application/v1/application_pb';
+	import CopyButton from '$lib/components/custom/copy-button/copy-button.svelte';
 	import { Cells } from '$lib/components/custom/data-table/core';
 	import * as Layout from '$lib/components/custom/data-table/layout';
 	import { TagGroup } from '$lib/components/tag-group';
 	import { Badge } from '$lib/components/ui/badge';
+	import * as Dialog from '$lib/components/ui/dialog';
 	import * as Tooltip from '$lib/components/ui/tooltip';
 	import { formatTimeAgo } from '$lib/formatter';
+	import { m } from '$lib/paraglide/messages';
 
 	export const cells = {
 		row_picker,
@@ -32,16 +33,42 @@
 
 {#snippet name(row: Row<Secret>)}
 	<Layout.Cell class="items-start">
-		<a
-			class="underline hover:no-underline"
-			href={resolve('/(auth)/scope/[scope]/applications/workloads/[namespace]/[application_name]', {
-				scope: page.params.scope!,
-				namespace: row.original.namespace!,
-				application_name: row.original.name!
-			})}
-		>
-			{row.original.name}
-		</a>
+		<Dialog.Root>
+			<Dialog.Trigger>
+				<p class="cursor-default underline hover:no-underline">
+					{row.original.name}
+				</p>
+			</Dialog.Trigger>
+			<Dialog.Content class="overflow-y-auto">
+				<Dialog.Header>
+					<Dialog.Title>
+						<h1 class="text-center text-2xl">{m.secrets()}</h1>
+					</Dialog.Title>
+				</Dialog.Header>
+				<div class="grid gap-4">
+					{#each Object.entries(row.original.data) as [key, value] (key)}
+						{@const secret = btoa(String.fromCharCode(...value))}
+						<div class="group flex w-full items-center gap-2">
+							<span
+								class="group-hover:bg-text-card rounded-full bg-muted p-2 transition-colors duration-200 group-hover:bg-muted-foreground"
+							>
+								<Icon icon="ph:key" class="size-5" />
+							</span>
+							<div class="w-full space-y-1">
+								<h3 class="text-xs font-medium text-muted-foreground">{key}</h3>
+								<span class="flex items-center gap-1">
+									<p class="max-w-sm truncate text-sm">{secret}</p>
+									<CopyButton
+										class="ml-auto size-4 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+										text={secret}
+									/>
+								</span>
+							</div>
+						</div>
+					{/each}
+				</div>
+			</Dialog.Content>
+		</Dialog.Root>
 	</Layout.Cell>
 {/snippet}
 
