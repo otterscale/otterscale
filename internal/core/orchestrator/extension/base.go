bbb6368c6cab2d4f4e2f59eb8fbf99917a660652
Paul Tsai <paul_tsai@phison.com>
2025-11-20 10:00:07 +0000

    feat: add CustomResourceDefinition repository implementation

--- internal/core/orchestrator/extension/base.go
+++ internal/core/orchestrator/extension/base.go
@@ -8,75 +8,135 @@ import (
 
 const chartRepoURL = "https://otterscale.github.io/charts"
 
-type base struct {
-	Name        string
+type chartManifest struct {
 	Namespace   string
-	Version     string
 	RepoURL     string
 	Labels      map[string]string
 	Annotations map[string]string
 	ValuesMap   map[string]string
 }
 
+type crdManifest struct {
+	Version              string
+	RepoURL              string
+	AnnotationVersionKey string
+}
+
+type base struct {
+	ID          string
+	Name        string
+	Description string
+	Logo        string
+	Chart       *chartManifest
+	CRD         *crdManifest
+}
+
 var (
 	general = []base{
 		{
-			Name:      "kube-prometheus-stack",
-			Namespace: "monitoring",
-			RepoURL:   "https://prometheus-community.github.io/helm-charts",
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "gateway-api",
+			Name:        "Gateway API",
+			Description: "Gateway API is an official Kubernetes project focused on L4 and L7 routing in Kubernetes.",
+			Logo:        "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/refs/tags/v1.3.0/site-src/images/logo/logo.svg",
+			CRD: &crdManifest{
+				Version:              "v1.3.0",
+				RepoURL:              "https://github.com/kubernetes-sigs/gateway-api.git/config/crd",
+				AnnotationVersionKey: "gateway.networking.k8s.io/bundle-version",
+			},
+		},
+		{
+			ID:          "gateway-api-inference-extension",
+			Name:        "Gateway API Inference Extension",
+			Description: "Gateway API Inference Extension is an official Kubernetes project that optimizes self-hosting Generative Models on Kubernetes.",
+			Logo:        "",
+			CRD: &crdManifest{
+				Version:              "v1.1.0",
+				RepoURL:              "https://github.com/kubernetes-sigs/gateway-api-inference-extension.git/config/crd",
+				AnnotationVersionKey: "inference.networking.k8s.io/bundle-version",
+			},
+		},
+		{
+			ID:          "kube-prometheus-stack",
+			Name:        "Prometheus Stack",
+			Description: "Installs the kube-prometheus stack for easy, end-to-end Kubernetes cluster monitoring using the Prometheus Operator.",
+			Logo:        "https://github.com/prometheus-community.png",
+			Chart: &chartManifest{
+				Namespace: "monitoring",
+				RepoURL:   "https://prometheus-community.github.io/helm-charts",
+				ValuesMap: map[string]string{
+					release.TypeLabel: "extension",
+				},
 			},
 		},
 	}
 
 	model = []base{
 		{
-			Name:      "gpu-operator",
-			Namespace: "nvidia-gpu-operator",
-			RepoURL:   "https://nvidia.github.io/gpu-operator",
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "gpu-operator",
+			Name:        "NVIDIA GPU Operator",
+			Description: "NVIDIA GPU Operator creates, configures, and manages GPUs in Kubernetes.",
+			Logo:        "https://github.com/nvidia.png",
+			Chart: &chartManifest{
+				Namespace: "nvidia-gpu-operator",
+				RepoURL:   "https://nvidia.github.io/gpu-operator",
+				ValuesMap: map[string]string{
+					release.TypeLabel: "extension",
+				},
 			},
 		},
 		{
-			Name:      "llm-d-infra",
-			Namespace: "llm-d",
-			RepoURL:   "https://llm-d-incubation.github.io/llm-d-infra",
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
-				"nameOverride":    "llm-d-infra",
-				"gateway.gatewayParameters.resources.limits.cpu":    "4",
-				"gateway.gatewayParameters.resources.limits.memory": "2Gi",
-				"gateway.service.type":                              "NodePort",
+			ID:          "llm-d-infra",
+			Name:        "llm-d",
+			Description: "Achieve state of the art inference performance with modern accelerators on Kubernetes.",
+			Logo:        "https://github.com/llm-d.png",
+			Chart: &chartManifest{
+				Namespace: "llm-d",
+				RepoURL:   "https://llm-d-incubation.github.io/llm-d-infra",
+				ValuesMap: map[string]string{
+					release.TypeLabel: "extension",
+					"nameOverride":    "llm-d-infra",
+					"gateway.gatewayParameters.resources.limits.cpu":    "4",
+					"gateway.gatewayParameters.resources.limits.memory": "2Gi",
+					"gateway.service.type":                              "NodePort",
+				},
 			},
 		},
 	}
 
 	instance = []base{
 		{
-			Name:      "kubevirt-infra",
-			Namespace: "kubevirt",
-			RepoURL:   chartRepoURL,
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "kubevirt-infra",
+			Name:        "KubeVirt",
+			Description: "Kubernetes Virtualization API and runtime in order to define and manage virtual machines.",
+			Logo:        "https://github.com/kubevirt.png",
+			Chart: &chartManifest{
+				Namespace: "kubevirt",
+				RepoURL:   chartRepoURL,
+				ValuesMap: map[string]string{
+					release.TypeLabel: "extension",
+				},
 			},
 		},
 	}
 
 	storage = []base{
 		{
-			Name:      "samba-operator",
-			Namespace: "samba-operator",
-			RepoURL:   chartRepoURL,
-			ValuesMap: map[string]string{
-				release.TypeLabel: "extension",
+			ID:          "samba-operator",
+			Name:        "Samba",
+			Description: "An operator for Samba as a service on PVCs in Kubernetes.",
+			Logo:        "",
+			Chart: &chartManifest{
+				Namespace: "samba-operator",
+				RepoURL:   chartRepoURL,
+				ValuesMap: map[string]string{
+					release.TypeLabel: "extension",
+				},
 			},
 		},
 	}
 )
 
-func (uc *UseCase) base(name string) (base, error) {
+func (uc *UseCase) base(id string) (base, error) {
 	all := []base{}
 	all = append(all, general...)
 	all = append(all, model...)
@@ -84,10 +144,10 @@ func (uc *UseCase) base(name string) (base, error) {
 	all = append(all, storage...)
 
 	for _, p := range all {
-		if p.Name == name {
+		if p.ID == id {
 			return p, nil
 		}
 	}
 
-	return base{}, fmt.Errorf("extension %s not found", name)
+	return base{}, fmt.Errorf("extension %s not found", id)
 }
--- internal/core/orchestrator/extension/extension.go
+++ internal/core/orchestrator/extension/extension.go
@@ -4,29 +4,47 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
+	"github.com/goccy/go-yaml"
 	"golang.org/x/sync/errgroup"
 	"helm.sh/helm/v3/pkg/storage/driver"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"sigs.k8s.io/kustomize/api/krusty"
+	"sigs.k8s.io/kustomize/api/resmap"
+	"sigs.k8s.io/kustomize/kyaml/filesys"
 
 	"github.com/otterscale/otterscale/internal/core/application/chart"
+	"github.com/otterscale/otterscale/internal/core/application/cluster"
 	"github.com/otterscale/otterscale/internal/core/application/release"
 )
 
+type Manifest struct {
+	ID      string
+	Version string
+}
+
 type Extension struct {
-	Release   *release.Release
-	Latest    *chart.Version
-	LatestURL string
+	Name        string
+	Description string
+	Icon        string
+	Status      string
+	DeployedAt  *time.Time
+	Current     *Manifest
+	Latest      *Manifest
 }
 
 type UseCase struct {
-	chart   chart.ChartRepo
-	release release.ReleaseRepo
+	chart                    chart.ChartRepo
+	customResourceDefinition cluster.CustomResourceDefinitionRepo
+	release                  release.ReleaseRepo
 }
 
-func NewUseCase(chart chart.ChartRepo, release release.ReleaseRepo) *UseCase {
+func NewUseCase(chart chart.ChartRepo, customResourceDefinition cluster.CustomResourceDefinitionRepo, release release.ReleaseRepo) *UseCase {
 	return &UseCase{
-		chart:   chart,
-		release: release,
+		chart:                    chart,
+		customResourceDefinition: customResourceDefinition,
+		release:                  release,
 	}
 }
 
@@ -46,36 +64,120 @@ func (uc *UseCase) ListStorageExtensions(ctx context.Context, scope string) ([]E
 	return uc.listExtensions(ctx, scope, storage)
 }
 
-func (uc *UseCase) InstallExtensions(ctx context.Context, scope string, chartRefMap map[string]string) error {
+func (uc *UseCase) InstallExtensions(ctx context.Context, scope string, manifests []Manifest) error {
 	eg, egctx := errgroup.WithContext(ctx)
 
-	for name, chartRef := range chartRefMap {
+	for _, manifest := range manifests {
 		eg.Go(func() error {
-			base, err := uc.base(name)
+			base, err := uc.base(manifest.ID)
 			if err != nil {
 				return err
 			}
 
-			_, err = uc.release.Install(egctx, scope, base.Namespace, base.Name, false, chartRef, base.Labels, base.Labels, base.Annotations, "", base.ValuesMap)
-			return err
+			chart := base.Chart
+			if chart != nil {
+				version, err := uc.chart.GetStableVersion(egctx, chart.RepoURL, base.ID, true)
+				if err != nil {
+					return err
+				}
+
+				if len(version.URLs) == 0 {
+					return fmt.Errorf("no chart url found for %q version %q", base.ID, manifest.Version)
+				}
+
+				chartRef := version.URLs[0]
+
+				_, err = uc.release.Install(egctx, scope, chart.Namespace, base.Name, false, chartRef, chart.Labels, chart.Labels, chart.Annotations, "", chart.ValuesMap)
+				return err
+			}
+
+			crd := base.CRD
+			if crd != nil {
+				fSys := filesys.MakeFsOnDisk()
+				k := krusty.MakeKustomizer(krusty.MakeDefaultOptions())
+
+				m, err := k.Run(fSys, uc.remoteCRDFormat(crd.RepoURL, manifest.Version))
+
+				for _, node := range m.Resources() {
+					data, err := node.AsYAML()
+					if err != nil {
+						return err
+					}
+
+					var crd *apiextensionsv1.CustomResourceDefinition
+					if err := yaml.Unmarshal(data, &crd); err != nil {
+						return err
+					}
+
+					if _, err := uc.customResourceDefinition.Create(ctx, scope, crd); err != nil {
+						return err
+					}
+				}
+
+				return err
+			}
+
+			return nil
 		})
 	}
 
 	return eg.Wait()
 }
 
-func (uc *UseCase) UpgradeExtensions(ctx context.Context, scope string, chartRefMap map[string]string) error {
+func (uc *UseCase) UpgradeExtensions(ctx context.Context, scope string, manifests []Manifest) error {
 	eg, egctx := errgroup.WithContext(ctx)
 
-	for name, chartRef := range chartRefMap {
+	for _, manifest := range manifests {
 		eg.Go(func() error {
-			base, err := uc.base(name)
+			base, err := uc.base(manifest.ID)
 			if err != nil {
 				return err
 			}
 
-			_, err = uc.release.Upgrade(egctx, scope, base.Namespace, base.Name, false, chartRef, "", base.ValuesMap, true)
-			return err
+			chart := base.Chart
+			if chart != nil {
+				version, err := uc.chart.GetStableVersion(egctx, chart.RepoURL, base.ID, true)
+				if err != nil {
+					return err
+				}
+
+				if len(version.URLs) == 0 {
+					return fmt.Errorf("no chart url found for %q version %q", base.ID, manifest.Version)
+				}
+
+				chartRef := version.URLs[0]
+
+				_, err = uc.release.Upgrade(egctx, scope, chart.Namespace, base.Name, false, chartRef, "", chart.ValuesMap, true)
+				return err
+			}
+
+			crd := base.CRD
+			if crd != nil {
+				fSys := filesys.MakeFsOnDisk()
+				k := krusty.MakeKustomizer(krusty.MakeDefaultOptions())
+
+				m, err := k.Run(fSys, uc.remoteCRDFormat(crd.RepoURL, manifest.Version))
+
+				for _, node := range m.Resources() {
+					data, err := node.AsYAML()
+					if err != nil {
+						return err
+					}
+
+					var crd *apiextensionsv1.CustomResourceDefinition
+					if err := yaml.Unmarshal(data, &crd); err != nil {
+						return err
+					}
+
+					if _, err := uc.customResourceDefinition.Update(ctx, scope, crd); err != nil {
+						return err
+					}
+				}
+
+				return err
+			}
+
+			return nil
 		})
 	}
 
@@ -85,27 +187,58 @@ func (uc *UseCase) UpgradeExtensions(ctx context.Context, scope string, chartRef
 func (uc *UseCase) listExtensions(ctx context.Context, scope string, bases []base) ([]Extension, error) {
 	versions := make([]chart.Version, len(bases))
 	releases := make([]release.Release, len(bases))
+	resMaps := make([]resmap.ResMap, len(bases))
+	crds := make([]cluster.CustomResourceDefinition, len(bases))
+
 	eg, egctx := errgroup.WithContext(ctx)
 
 	for i := range bases {
-		eg.Go(func() error {
-			v, err := uc.chart.GetStableVersion(egctx, bases[i].RepoURL, bases[i].Name, true)
-			if err == nil {
-				versions[i] = *v
-			}
-			return err
-		})
+		base := bases[i]
+		chart := base.Chart
+		crd := base.CRD
 
-		eg.Go(func() error {
-			v, err := uc.release.Get(egctx, scope, bases[i].Namespace, bases[i].Name)
-			if err == nil {
-				releases[i] = *v
-			}
-			if errors.Is(err, driver.ErrReleaseNotFound) {
-				return nil
-			}
-			return err
-		})
+		if chart != nil {
+			eg.Go(func() error {
+				v, err := uc.chart.GetStableVersion(egctx, chart.RepoURL, base.ID, true)
+				if err == nil {
+					versions[i] = *v
+				}
+				return err
+			})
+
+			eg.Go(func() error {
+				v, err := uc.release.Get(egctx, scope, chart.Namespace, base.ID)
+				if err == nil {
+					releases[i] = *v
+				}
+				if errors.Is(err, driver.ErrReleaseNotFound) {
+					return nil
+				}
+				return err
+			})
+		}
+
+		if crd != nil {
+			eg.Go(func() error {
+				v, err := uc.customResourceDefinition.List(egctx, scope, "")
+				if err == nil {
+					crds = v
+				}
+				return err
+			})
+
+			eg.Go(func() error {
+				fSys := filesys.MakeFsOnDisk()
+				k := krusty.MakeKustomizer(krusty.MakeDefaultOptions())
+
+				resMap, err := k.Run(fSys, uc.remoteCRDFormat(crd.RepoURL, crd.Version))
+				if err == nil {
+					resMaps[i] = resMap
+				}
+
+				return err
+			})
+		}
 	}
 
 	if err := eg.Wait(); err != nil {
@@ -115,16 +248,105 @@ func (uc *UseCase) listExtensions(ctx context.Context, scope string, bases []bas
 	ret := []Extension{}
 
 	for i := range bases {
-		if len(versions[i].URLs) == 0 {
-			return nil, fmt.Errorf("no chart URL found for %q", bases[i].Name)
+		chart := bases[i].Chart
+
+		if chart != nil {
+			ret = append(ret, *uc.buildExtensionFromChart(&bases[i], &releases[i], &versions[i]))
+
+			continue
 		}
 
-		ret = append(ret, Extension{
-			Release:   &releases[i],
-			Latest:    &versions[i],
-			LatestURL: versions[i].URLs[0],
-		})
+		crd := bases[i].CRD
+
+		if crd != nil {
+			if len(resMaps[i].Resources()) == 0 {
+				return nil, fmt.Errorf("no crd resource found for %q", bases[i].ID)
+			}
+
+			ret = append(ret, *uc.buildExtensionFromCRD(&bases[i], resMaps[i], crds, crd.AnnotationVersionKey))
+
+			continue
+		}
 	}
 
 	return ret, nil
+}
+
+func (uc *UseCase) buildExtensionFromChart(base *base, release *release.Release, version *chart.Version) *Extension {
+	var (
+		status     string
+		deployedAt *time.Time
+		current    *Manifest
+		latest     *Manifest
+	)
+
+	if release.Info != nil {
+		status = release.Info.Status.String()
+		deployedAt = &release.Info.LastDeployed.Time
+		current = &Manifest{
+			ID:      release.Chart.Metadata.Name,
+			Version: release.Chart.Metadata.Version,
+		}
+	}
+
+	if len(version.URLs) > 0 {
+		latest = &Manifest{
+			ID:      version.Name,
+			Version: version.Version,
+		}
+	}
+
+	return &Extension{
+		Name:        base.Name,
+		Description: base.Description,
+		Icon:        base.Logo,
+		Status:      status,
+		DeployedAt:  deployedAt,
+		Current:     current,
+		Latest:      latest,
+	}
+}
+
+func (uc *UseCase) buildExtensionFromCRD(base *base, resMap resmap.ResMap, crds []cluster.CustomResourceDefinition, annotationVersionKey string) *Extension {
+	var (
+		status     string
+		deployedAt *time.Time
+		current    *Manifest
+		latest     *Manifest
+	)
+
+	for i := range crds {
+		if version, ok := crds[i].Annotations[annotationVersionKey]; ok {
+			status = "deployed"
+			deployedAt = &crds[i].CreationTimestamp.Time
+			current = &Manifest{
+				ID:      base.ID,
+				Version: version,
+			}
+			break
+		}
+	}
+
+	annotations := resMap.Resources()[0].GetAnnotations()
+
+	if version, ok := annotations[annotationVersionKey]; ok {
+		latest = &Manifest{
+			ID:      base.ID,
+			Version: version,
+		}
+	}
+
+	return &Extension{
+		Name:        base.Name,
+		Description: base.Description,
+		Icon:        base.Logo,
+		Status:      status,
+		DeployedAt:  deployedAt,
+		Current:     current,
+		Latest:      latest,
+	}
+}
+
+func (uc *UseCase) remoteCRDFormat(repoURL, version string) string {
+	return fmt.Sprintf("%s?ref=%s", repoURL, version)
 }
