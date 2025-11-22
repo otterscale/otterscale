46a1a6cc16d01479a99e817e57ee4cb367978800
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-22 16:39:34 +0800

    fix: handle error in ValidateSMBUser and remove unused response function (#977)

--- internal/app/storage.go
+++ internal/app/storage.go
@@ -554,8 +554,12 @@ func (s *StorageService) ValidateSMBUser(_ context.Context, req *pb.ValidateSMBU
 		req.GetPassword(),
 		req.GetSearchUsername(),
 		req.GetTls())
+	if err != nil {
+		return nil, err
+	}
 
-	resp := toValidateSMBUserResponse(entityType, err)
+	resp := &pb.ValidateSMBUserResponse{}
+	resp.SetEntityType(toProtoEntityType(entityType))
 	return resp, nil
 }
 
@@ -1176,18 +1180,4 @@ func toProtoEntityType(et smb.EntityType) pb.ValidateSMBUserResponse_EntityType 
 	default:
 		return pb.ValidateSMBUserResponse_ENTITY_TYPE_UNKNOWN
 	}
-}
-
-func toValidateSMBUserResponse(et smb.EntityType, err error) *pb.ValidateSMBUserResponse {
-	valid := et != smb.EntityTypeUnknown
-
-	ret := &pb.ValidateSMBUserResponse{}
-	ret.SetValid(valid)
-	ret.SetEntityType(toProtoEntityType(et))
-
-	if err != nil {
-		ret.SetMessage(err.Error())
-	}
-
-	return ret
 }
