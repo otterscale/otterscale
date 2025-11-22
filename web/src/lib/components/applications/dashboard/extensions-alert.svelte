2eb10c95a8fb378104cb70d5a9140771fe5cb50d
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-22 21:28:48 +0800

    refactor: rename fields in Extension and Manifest messages

--- web/src/lib/components/applications/dashboard/extensions-alert.svelte
+++ web/src/lib/components/applications/dashboard/extensions-alert.svelte
@@ -55,7 +55,7 @@
 				<div class="flex w-full flex-wrap gap-2">
 					{#each $generalExtensions.filter((extension) => !extension.current) as extension}
 						<Badge variant="outline" class="border-destructive/50 bg-destructive/5 text-destructive"
-							>{extension.name}</Badge
+							>{extension.displayName}</Badge
 						>
 					{/each}
 				</div>
