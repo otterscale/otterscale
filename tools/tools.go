7fe2c3fbff84902d1eb5ee6a3511afafe41d03d3
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-25 13:23:33 +0800

    refactor: update go.mod and go.sum dependencies, remove unused tools package (#1017)

--- tools/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-
-// This package imports things required by wire, to force `go mod` to see them as dependencies
-package tools
-
-import (
-	_ "github.com/google/subcommands"
-	_ "golang.org/x/tools/cmd/stringer"
-)
