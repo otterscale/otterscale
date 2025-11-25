2a267261015c46890819334839c9680b3fc8a86d
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-25 16:48:38 +0800

    refactor: update Dockerfile and workflows to use version from release tag (#1024)

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		version: {
+			name: process.env.VERSION || 'devel'
+		}
+	},
 	extensions: ['.svelte', '.svx']
 };
 
