b238eb418d78d085faaaf8a9324e1c8f639f9edf
Wu Yi-Lin <terrywu25@gmail.com>
2025-11-26 20:00:21 +0800

    feat: skip os check

--- scripts/install.sh
+++ scripts/install.sh
@@ -1705,7 +1705,7 @@ main() {
     # System validation
     log "INFO" "Performing system validation..." "VALIDATION"
     check_root
-    check_os
+    #check_os
     check_memory
     check_disk
     disable_ipv6
