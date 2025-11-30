2b6baa6a3476dcfe09f5a2db8631802bb596c227
Wu Yi-Lin <terrywu25@gmail.com>
2025-11-30 21:31:00 +0900

    fix: kubectl patch error

--- scripts/install.sh
+++ scripts/install.sh
@@ -1351,15 +1351,19 @@ juju_add_k8s() {
     local CURRENT=$(microk8s kubectl get svc traefik-lb -n cos -o jsonpath='{.metadata.annotations.metallb\.io/LoadBalancerIPs}' 2>/dev/null || true)
     if [[ "$CURRENT" != "$OTTERSCALE_INTERFACE_IP" ]]; then
         log "INFO" "Specific metallb ip to service traefik-lb" "MICROK8S_SVC"
-        cat <<EOF | microk8s kubectl patch svc traefik-lb -n cos --type merge -f -
+        
+        microk8s kubectl patch svc traefik-lb -n cos \
+            --type merge \
+            -p "$(cat <<EOF
 {
-    "metadata": {
-        "annotations": {
-            "metallb.io/LoadBalancerIPs": "$OTTERSCALE_INTERFACE_IP"
-        }
+  "metadata": {
+    "annotations": {
+      "metallb.io/LoadBalancerIPs": "${OTTERSCALE_INTERFACE_IP}"
     }
+  }
 }
 EOF
+            )"
     fi
 
     log "INFO" "Kubernetes cluster added to Juju successfully" "JUJU_K8S"
@@ -1515,19 +1519,23 @@ deploy_helm() {
     install_helm_chart "istiod" "istio-system" "istio/istiod" "--wait --timeout 10m"
     install_helm_chart "istiod-ingress" "istio-system" "istio/gateway" "-n istio-system --wait --timeout 10m"
 
-    # Patch traefik-lb
+    # Patch istiod-ingress
     local CURRENT=$(microk8s kubectl get svc istiod-ingress -n istio-system -o jsonpath='{.metadata.annotations.metallb\.io/LoadBalancerIPs}' 2>/dev/null || true)
     if [[ "$CURRENT" != "$OTTERSCALE_WEB_IP" ]]; then
-        log "INFO" "Specific metallb ip to service istiod-ingress" "MICROK8S_SVC"
-        cat <<EOF | microk8s kubectl patch svc istiod-ingress -n istiod-ingress --type merge -f -
+        log "INFO" "Specific metallb ip to service traefik-lb" "MICROK8S_SVC"
+        
+        microk8s kubectl patch svc istiod-ingress -n istiod-ingress \
+            --type merge \
+            -p "$(cat <<EOF
 {
-    "metadata": {
-        "annotations": {
-            "metallb.io/LoadBalancerIPs": "$OTTERSCALE_WEB_IP"
-        }
+  "metadata": {
+    "annotations": {
+      "metallb.io/LoadBalancerIPs": "${OTTERSCALE_WEB_IP}"
     }
+  }
 }
 EOF
+            )"
     fi
 
     install_helm_chart "cert-manager" "cert-manager" "jetstack/cert-manager" "--create-namespace --version v1.19.1 --set crds.enabled=true --wait --timeout 10m"
