8333e268c6f95c4493c6df96a0f20a720dcf1469
Wu Yi-Lin <terrywu25@gmail.com>
2025-12-10 15:27:07 +0800

    fix: model latency metrics

--- web/src/lib/components/models/dashboard/overview/latency.svelte
+++ web/src/lib/components/models/dashboard/overview/latency.svelte
@@ -36,9 +36,9 @@
 	async function fetchLatestLatency() {
 		try {
 			const response = await prometheusDriver.instantQuery(
-				`histogram_quantile(0.95, sum by(le) (vllm:e2e_request_latency_seconds_bucket{juju_model="${scope}"})) * 1000`
+				`histogram_quantile(0.95, sum by(le) (vllm:e2e_request_latency_seconds_bucket{juju_model="${scope}"}))`
 			);
-			latestLatency = response.result[0]?.value ? Number(response.result[0]?.value) : undefined;
+			latestLatency = response.result[0]?.value?. value;
 		} catch (error) {
 			console.error(`Fail to fetch latest latency in scope ${scope}:`, error);
 		}
@@ -47,7 +47,7 @@
 	async function fetchLatencies() {
 		try {
 			const response = await prometheusDriver.rangeQuery(
-				`histogram_quantile(0.95, sum by(le) (rate(vllm:e2e_request_latency_seconds_bucket{juju_model="${scope}"}[5m]))) * 1000`,
+				`histogram_quantile(0.95, sum by(le) (rate(vllm:e2e_request_latency_seconds_bucket{juju_model="${scope}"}[2m])))`,
 				Date.now() - 10 * 60 * 1000,
 				Date.now(),
 				2 * 60
@@ -125,7 +125,7 @@
 		<Card.Content class="flex flex-wrap items-center justify-between gap-6">
 			<div class="flex flex-col gap-0.5">
 				<div class="text-3xl font-bold">{latestLatency.toFixed(2)}</div>
-				<p class="text-sm text-muted-foreground">{m.millisecond()}</p>
+				<p class="text-sm text-muted-foreground">{m.second()}</p>
 			</div>
 			<Chart.Container config={configuration} class="h-full w-20">
 				<LineChart
@@ -161,7 +161,7 @@
 									<div class="grid gap-1.5">
 										<span class="text-muted-foreground">{name}</span>
 									</div>
-									<p class="font-mono">{Number(value).toFixed(2)} {m.millisecond()}</p>
+									<p class="font-mono">{Number(value).toFixed(2)} {m.second()}</p>
 								</div>
 							{/snippet}
 						</Chart.Tooltip>
