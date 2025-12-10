ad4bbe408a5104e8035af764d62500ba17bdb7ab
woody_lin <jj7158761@gmail.com>
2025-12-10 14:04:05 +0800

    feat: update boot image import button icons to `box-arrow-down` and refine importing animation.

--- web/src/lib/components/settings/boot-image/import.svelte
+++ web/src/lib/components/settings/boot-image/import.svelte
@@ -66,10 +66,11 @@
 	class="flex items-center gap-2"
 >
 	{#if isImportingBootImages == true}
-		<Icon icon="ph:spinner" class="size-5 animate-spin text-muted-foreground" />
+		<Icon icon="ph:box-arrow-down" class="size-5 animate-pulse text-muted-foreground" />
+
 		{m.importing()}
 	{:else}
-		<Icon icon="ph:arrows-clockwise" />
+		<Icon icon="ph:box-arrow-down" class="size-5" />
 		{m.import()}
 	{/if}
 </Button>
