1aac372540a4a493dfe22d0d11c7474b68bb7c15
enyao_chang <zhang_enyao@icloud.com>
2025-12-10 16:18:54 +0800

    feat: Fix application text alignment and update synchronization to include chart data

--- web/src/lib/components/applications/store/commerce-store/thumbnail.svelte
+++ web/src/lib/components/applications/store/commerce-store/thumbnail.svelte
@@ -6,6 +6,7 @@
 	import * as Avatar from '$lib/components/ui/avatar';
 	import { Badge } from '$lib/components/ui/badge';
 	import * as Card from '$lib/components/ui/card';
+	import * as Item from '$lib/components/ui/item/index.js';
 	import { m } from '$lib/paraglide/messages';
 	import { cn } from '$lib/utils';
 
@@ -16,32 +17,29 @@
 	let { chart, chartReleases }: { chart: Chart; chartReleases: Release[] | undefined } = $props();
 </script>
 
-<Card.Root
-	class={cn(
-		chart.deprecated ? 'bg-muted' : 'hover:shadow-lg',
-		'relative flex h-50 flex-col gap-4 overflow-hidden transition-all'
-	)}
->
+<Card.Root class={cn(chart.deprecated ? 'bg-muted' : 'hover:shadow-lg', 'relative h-52 gap-0')}>
 	<Card.Header>
 		{#if chart.repositoryName.startsWith('otterscale/')}
 			<span class="absolute top-6 right-6">
 				<Icon icon="ph:star-fill" class="h-4 w-4 fill-yellow-400 text-yellow-400" />
 			</span>
 		{/if}
-		<div class="flex items-center gap-2">
-			<Avatar.Root class="h-12 w-12">
+		<Item.Root class="p-0">
+			<Avatar.Root class="m-2 size-8">
 				<Avatar.Image src={chart.icon} class="object-contain" />
 				<Avatar.Fallback>
 					<Icon icon={fuzzLogosIcon(chart.name, 'fluent-emoji-flat:otter')} class="size-12" />
 				</Avatar.Fallback>
 			</Avatar.Root>
-			<span>
-				<h3 class="font-semibold">{chart.name}</h3>
-				<p class="flex items-center gap-1 text-sm text-muted-foreground">
+			<Item.Content class="text-left">
+				<Item.Title>
+					{chart.name}
+				</Item.Title>
+				<Item.Description>
 					{chart.version}
-				</p>
-			</span>
-		</div>
+				</Item.Description>
+			</Item.Content>
+		</Item.Root>
 	</Card.Header>
 	<Card.Content class="px-8 py-4 text-sm text-muted-foreground">
 		<p class="line-clamp-3 text-left">{chart.description}</p>
