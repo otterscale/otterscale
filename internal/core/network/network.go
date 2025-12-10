0b5e565fad8204be1a71c66c89e3de587a645a56
Shawn Tsai <shawn_tsai@phison.com>
2025-12-10 15:08:51 +0800

    Use defer

--- internal/core/network/network.go
+++ internal/core/network/network.go
@@ -90,27 +90,32 @@ func (uc *UseCase) CreateNetwork(ctx context.Context, cidr, gatewayIP string, dn
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		if err != nil {
+			_ = uc.fabric.Delete(ctx, fabric.ID)
+		}
+	}()
 
 	vlan := fabric.VLANs[0] // default VLAN
 
 	subnet, err := uc.subnet.Create(ctx, fabric.ID, vlan.ID, cidr, gatewayIP, dnsServers)
 	if err != nil {
-		_ = uc.fabric.Delete(ctx, fabric.ID)
 		return nil, err
 	}
+	defer func() {
+		if err != nil {
+			_ = uc.subnet.Delete(ctx, subnet.ID)
+		}
+	}()
 
 	if dhcpOn {
 		if _, err := uc.vlan.Update(ctx, fabric.ID, vlan.VID, vlan.Name, vlan.MTU, vlan.Description, true); err != nil {
-			_ = uc.subnet.Delete(ctx, subnet.ID)
-			_ = uc.fabric.Delete(ctx, fabric.ID)
 			return nil, err
 		}
 	}
 
 	subnetData, err := uc.getSubnetData(ctx, subnet)
 	if err != nil {
-		_ = uc.subnet.Delete(ctx, subnet.ID)
-		_ = uc.fabric.Delete(ctx, fabric.ID)
 		return nil, err
 	}
 
