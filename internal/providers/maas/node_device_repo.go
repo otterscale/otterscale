c851d121a04ca78b53ecee5b24e488919bcd452a
Wu Yi-Lin <terrywu25@gmail.com>
2025-12-11 18:04:56 +0800

    feat: filter gpu by vendors

--- internal/providers/maas/node_device_repo.go
+++ internal/providers/maas/node_device_repo.go
@@ -22,6 +22,13 @@ func NewNodeDeviceRepo(maas *MAAS) machine.NodeDeviceRepo {
 var _ machine.NodeDeviceRepo = (*nodeDeviceRepo)(nil)
 
 func (r *nodeDeviceRepo) ListGPUs(_ context.Context, machineID string) ([]machine.GPU, error) {
+	// Pre-defined allowed GPU vendors with their PCI vendor IDs
+	allowedVendors := map[string]bool{
+		"10de": true, // NVIDIA
+		"1002": true, // AMD
+		"8086": true, // Intel
+	}
+
 	client, err := r.maas.Client()
 	if err != nil {
 		return nil, err
@@ -31,5 +38,18 @@ func (r *nodeDeviceRepo) ListGPUs(_ context.Context, machineID string) ([]machin
 		HardwareType: "gpu",
 	}
 
-	return client.NodeDevices.Get(machineID, params)
+	nodeGPUs, err := client.NodeDevices.Get(machineID, params)
+	if err != nil {
+		return nil, err
+	}
+
+	// Pre-allocate slice with estimated capacity to reduce memory allocations
+	filteredGPUs := make([]entity.NodeDevice, 0, len(nodeGPUs))
+	for _, gpu := range nodeGPUs {
+		if allowedVendors[gpu.VendorID] {
+			filteredGPUs = append(filteredGPUs, gpu)
+		}
+	}
+
+	return filteredGPUs, nil
 }
