935d3bd7387b36ce949e8ee11a180198dc314da7
Woody Lin <jj7158761@gmail.com>
2025-12-17 20:59:57 +0800

    feat: display newly created access key and secret key in a modal and update backend to return secret key only on creation. (#1302)

--- web/src/lib/components/storage/object-gateway/user/key/data-table/action-create.svelte
+++ web/src/lib/components/storage/object-gateway/user/key/data-table/action-create.svelte
@@ -4,8 +4,10 @@
 	import { getContext } from 'svelte';
 	import { toast } from 'svelte-sonner';
 
-	import type { CreateUserKeyRequest, User } from '$lib/api/storage/v1/storage_pb';
+	import type { User, User_Key } from '$lib/api/storage/v1/storage_pb';
 	import { StorageService } from '$lib/api/storage/v1/storage_pb';
+	import CopyButton from '$lib/components/custom/copy-button/copy-button.svelte';
+	import { SingleStep as Modal } from '$lib/components/custom/modal';
 	import type { ReloadManager } from '$lib/components/custom/reloader';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { m } from '$lib/paraglide/messages';
@@ -25,37 +27,89 @@
 
 	const storageClient = createClient(StorageService, transport);
 
-	const defaults = {
-		scope: scope,
-		userId: user.id
-	} as CreateUserKeyRequest;
-	let request = $state(defaults);
-	function reset() {
-		request = defaults;
-	}
+	let createdKey = $state<User_Key | undefined>(undefined);
+	let resultOpen = $state(false);
 </script>
 
 <Button
 	class="size-sm flex h-full w-full items-center gap-2 capitalize"
 	onclick={() => {
-		toast.promise(() => storageClient.createUserKey(request), {
-			loading: `Creating access key...`,
-			success: () => {
-				reloadManager.force();
-				return `Create access key`;
-			},
-			error: (error) => {
-				let message = `Fail to create access key`;
-				toast.error(message, {
-					description: (error as ConnectError).message.toString(),
-					duration: Number.POSITIVE_INFINITY
-				});
-				return message;
+		toast.promise(
+			() =>
+				storageClient.createUserKey({
+					scope: scope,
+					userId: user.id
+				}),
+			{
+				loading: `Creating access key...`,
+				success: (key) => {
+					reloadManager.force();
+					createdKey = key;
+					resultOpen = true;
+					return `Create access key`;
+				},
+				error: (error) => {
+					let message = `Fail to create access key`;
+					toast.error(message, {
+						description: (error as ConnectError).message.toString(),
+						duration: Number.POSITIVE_INFINITY
+					});
+					return message;
+				}
 			}
-		});
-		reset();
+		);
 	}}
 >
 	<Icon icon="ph:plus" />
 	{m.create()}
 </Button>
+
+<Modal.Root bind:open={resultOpen}>
+	<Modal.Content>
+		<Modal.Header>{m.created_access_key()}</Modal.Header>
+		{#if createdKey}
+			<div class="grid gap-4">
+				<div class="group flex w-full items-center gap-2">
+					<span
+						class="group-hover:bg-text-card rounded-full bg-muted p-2 transition-colors duration-200 group-hover:bg-muted-foreground"
+					>
+						<Icon icon="ph:key" class="size-5" />
+					</span>
+					<div class="w-full space-y-1">
+						<h3 class="text-xs font-medium text-muted-foreground">{m.access_key()}</h3>
+						<span class="flex items-center gap-1">
+							<p class="max-w-sm truncate text-sm">{createdKey.accessKey}</p>
+							<CopyButton
+								class="ml-auto size-4 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+								text={createdKey.accessKey}
+							/>
+						</span>
+					</div>
+				</div>
+
+				<div class="group flex w-full items-center gap-2">
+					<span
+						class="group-hover:bg-text-card rounded-full bg-muted p-2 transition-colors duration-200 group-hover:bg-muted-foreground"
+					>
+						<Icon icon="ph:lock-key" class="size-5" />
+					</span>
+					<div class="w-full space-y-1">
+						<h3 class="text-xs font-medium text-muted-foreground">{m.secret_key()}</h3>
+						<span class="flex items-center gap-1">
+							<p class="max-w-sm truncate text-sm">{createdKey.secretKey}</p>
+							<CopyButton
+								class="ml-auto size-4 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+								text={createdKey.secretKey}
+							/>
+						</span>
+					</div>
+				</div>
+			</div>
+		{/if}
+		<Modal.Footer>
+			<Modal.Action onclick={() => (resultOpen = false)}>
+				{m.close()}
+			</Modal.Action>
+		</Modal.Footer>
+	</Modal.Content>
+</Modal.Root>
