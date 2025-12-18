11e39060ae81afe9ae6d388f365bd1da80807f1b
woody_lin <jj7158761@gmail.com>
2025-12-18 20:20:45 +0800

    refactor: Consolidate test creation form state by introducing a `warpInput` object and initializing it on modal open.

--- web/src/lib/components/settings/built-in-self-test/object-storage-test/data-table/action-test.svelte
+++ web/src/lib/components/settings/built-in-self-test/object-storage-test/data-table/action-test.svelte
@@ -16,6 +16,7 @@
 	} from '$lib/api/configuration/v1/configuration_pb';
 	import {
 		ConfigurationService,
+		InternalObjectService_Type,
 		Warp_Input_Operation
 	} from '$lib/api/configuration/v1/configuration_pb';
 	import * as Form from '$lib/components/custom/form';
@@ -65,55 +66,41 @@
 		closeActions?: () => void;
 	} = $props();
 
-	// Request
-	const DEFAULT_WARP_REQUEST = testResult
-		? ({
-				target: {
-					value: testResult.kind.value?.target.value,
-					case: testResult.kind.value?.target.case
-				}
-			} as Warp)
-		: ({ target: { value: {}, case: {} } } as Warp);
-	const DEFAULT_REQUEST = {
-		kind: { value: DEFAULT_WARP_REQUEST, case: 'warp' },
-		createdBy: page.data.user?.username ?? ''
-	} as CreateTestResultRequest;
-	const DEFAULT_INTERNAL_OBJECT_SERVICE =
-		testResult && testResult.kind.value?.target?.case === 'internalObjectService'
-			? (testResult.kind.value.target.value as InternalObjectService)
-			: ({} as InternalObjectService);
-	const DEFAULT_DEFAULT_EXTERNAL_OBJECT_SERVICE =
-		testResult && testResult.kind.value?.target?.case === 'externalObjectService'
-			? (testResult.kind.value.target.value as ExternalObjectService)
-			: ({} as ExternalObjectService);
-	const DEFAULT_WARP_INPUT =
-		testResult && testResult.kind.value?.input
-			? (testResult.kind.value.input as Warp_Input)
-			: ({
-					durationSeconds: 60,
-					objectSizeBytes: 4 * 1024 * 1024,
-					objectCount: 500
-				} as unknown as Warp_Input);
+	let request: CreateTestResultRequest = $state({} as CreateTestResultRequest);
+	let requestWarp: Warp = $state({} as Warp);
+	let requestInternalObjectService: InternalObjectService = $state({} as InternalObjectService);
+	let requestExternalObjectService: ExternalObjectService = $state({} as ExternalObjectService);
+	let warpInput = $state({} as Warp_Input);
 
-	let request: CreateTestResultRequest = $state(DEFAULT_REQUEST);
-	let requestWarp: Warp = $state(DEFAULT_WARP_REQUEST);
-	let requestInternalObjectService: InternalObjectService = $state(DEFAULT_INTERNAL_OBJECT_SERVICE);
-	let requestExternalObjectService: ExternalObjectService = $state(
-		DEFAULT_DEFAULT_EXTERNAL_OBJECT_SERVICE
-	);
-	let warpOperation = $state(DEFAULT_WARP_INPUT.operation);
-	let warpDuration = $state(DEFAULT_WARP_INPUT.durationSeconds);
-	let warpObjectSize = $state(DEFAULT_WARP_INPUT.objectSizeBytes);
-	let warpObjectCount = $state(DEFAULT_WARP_INPUT.objectCount);
-	function reset() {
-		request = DEFAULT_REQUEST;
-		requestWarp = DEFAULT_WARP_REQUEST;
-		requestInternalObjectService = DEFAULT_INTERNAL_OBJECT_SERVICE;
-		requestExternalObjectService = DEFAULT_DEFAULT_EXTERNAL_OBJECT_SERVICE;
-		warpOperation = DEFAULT_WARP_INPUT.operation;
-		warpDuration = DEFAULT_WARP_INPUT.durationSeconds;
-		warpObjectSize = DEFAULT_WARP_INPUT.objectSizeBytes;
-		warpObjectCount = DEFAULT_WARP_INPUT.objectCount;
+	function init() {
+		request = {
+			kind: { value: {} as Warp, case: 'warp' },
+			createdBy: page.data.user?.username ?? ''
+		} as CreateTestResultRequest;
+		requestWarp = testResult
+			? ({
+					target: {
+						value: testResult.kind.value?.target.value,
+						case: testResult.kind.value?.target.case
+					}
+				} as Warp)
+			: ({ target: { value: {}, case: {} } } as Warp);
+		requestInternalObjectService =
+			testResult && testResult.kind.value?.target?.case === 'internalObjectService'
+				? (testResult.kind.value.target.value as InternalObjectService)
+				: ({} as InternalObjectService);
+		requestExternalObjectService =
+			testResult && testResult.kind.value?.target?.case === 'externalObjectService'
+				? (testResult.kind.value.target.value as ExternalObjectService)
+				: ({} as ExternalObjectService);
+		warpInput =
+			testResult && testResult.kind.value?.input
+				? (testResult.kind.value.input as Warp_Input)
+				: ({
+						durationSeconds: 60,
+						objectSizeBytes: 4 * 1024 * 1024,
+						objectCount: 500
+					} as unknown as Warp_Input);
 	}
 
 	// Modal state
@@ -145,6 +132,11 @@
 <Modal.Root
 	bind:open
 	steps={3}
+	onOpenChange={(isOpen) => {
+		if (isOpen) {
+			init();
+		}
+	}}
 	onOpenChangeComplete={(isOpen) => {
 		if (closeActions && !isOpen) {
 			closeActions();
@@ -291,7 +283,7 @@
 								<SingleSelect.Root
 									options={warpInputOperation}
 									required
-									bind:value={warpOperation}
+									bind:value={warpInput.operation}
 									bind:invalid={invalidWarpInput.operation}
 								>
 									<SingleSelect.Trigger />
@@ -321,7 +313,7 @@
 							<Form.Field>
 								<Form.Label>{m.duration()}</Form.Label>
 								<SingleInput.Measurement
-									bind:value={warpDuration}
+									bind:value={warpInput.durationSeconds}
 									units={[
 										{ value: 1, label: 's' } as SingleInput.UnitType,
 										{ value: 60, label: 'm' } as SingleInput.UnitType,
@@ -334,7 +326,7 @@
 							<Form.Field>
 								<Form.Label>{m.object_size()}</Form.Label>
 								<SingleInput.Measurement
-									bind:value={warpObjectSize}
+									bind:value={warpInput.objectSizeBytes}
 									units={[
 										{ value: Math.pow(2, 10 * 0), label: 'B' } as SingleInput.UnitType,
 										{ value: Math.pow(2, 10 * 1), label: 'KB' } as SingleInput.UnitType,
@@ -351,8 +343,8 @@
 								<SingleInput.General
 									type="number"
 									placeholder="500"
-									bind:value={warpObjectCount}
-									disabled={warpOperation == Warp_Input_Operation.PUT}
+									bind:value={warpInput.objectCount}
+									disabled={warpInput.operation == Warp_Input_Operation.PUT}
 								/>
 							</Form.Field>
 						</Form.Fieldset>
@@ -368,7 +360,11 @@
 							<Form.Description>{m.name()}: {request.name}</Form.Description>
 							<Form.Description>{m.target()}: {requestWarp.target.case}</Form.Description>
 							{#if requestWarp.target.case == 'internalObjectService'}
-								<Form.Description>{m.type()}: {requestInternalObjectService.type}</Form.Description>
+								<Form.Description
+									>{m.type()}: {InternalObjectService_Type[
+										requestInternalObjectService.type
+									]}</Form.Description
+								>
 								<Form.Description
 									>{m.scope()}: {requestInternalObjectService.scope}</Form.Description
 								>
@@ -385,18 +381,18 @@
 						</Form.Fieldset>
 						<!-- Step 2 -->
 						<Form.Fieldset>
-							{@const duration = formatSecond(Number(warpDuration))}
-							{@const objectSize = formatCapacity(Number(warpObjectSize))}
+							{@const duration = formatSecond(Number(warpInput.durationSeconds))}
+							{@const objectSize = formatCapacity(Number(warpInput.objectSizeBytes))}
 							<Form.Legend>{m.advance()}</Form.Legend>
 							<Form.Description
-								>{m.operation()}: {Warp_Input_Operation[warpOperation]}</Form.Description
+								>{m.operation()}: {Warp_Input_Operation[warpInput.operation]}</Form.Description
 							>
 							<Form.Description>{m.duration()}: {duration.value} {duration.unit}</Form.Description>
 							<Form.Description
 								>{m.object_size()}: {objectSize.value} {objectSize.unit}</Form.Description
 							>
-							{#if warpOperation !== Warp_Input_Operation.PUT}
-								<Form.Description>{m.object_count()}: {warpObjectCount}</Form.Description>
+							{#if warpInput.operation !== Warp_Input_Operation.PUT}
+								<Form.Description>{m.object_count()}: {warpInput.objectCount}</Form.Description>
 							{/if}
 						</Form.Fieldset>
 					</Form.Root>
@@ -405,11 +401,7 @@
 		</Modal.Stepper>
 
 		<Modal.Footer>
-			<Modal.Cancel
-				onclick={() => {
-					reset();
-				}}>{m.cancel()}</Modal.Cancel
-			>
+			<Modal.Cancel>{m.cancel()}</Modal.Cancel>
 			<Modal.Controllers>
 				<Modal.Back>{m.back()}</Modal.Back>
 				<Modal.Next>{m.next()}</Modal.Next>
@@ -423,10 +415,13 @@
 							requestWarp.target.value = requestExternalObjectService;
 						}
 						requestWarp.input = {
-							operation: warpOperation,
-							durationSeconds: BigInt(warpDuration),
-							objectSizeBytes: BigInt(warpObjectSize),
-							objectCount: warpOperation === Warp_Input_Operation.PUT ? 0n : BigInt(warpObjectCount)
+							operation: warpInput.operation,
+							durationSeconds: BigInt(warpInput.durationSeconds),
+							objectSizeBytes: BigInt(warpInput.objectSizeBytes),
+							objectCount:
+								warpInput.operation === Warp_Input_Operation.PUT
+									? 0n
+									: BigInt(warpInput.objectCount)
 						} as Warp_Input;
 						request.kind.value = requestWarp;
 						toast.promise(() => client.createTestResult(request), {
@@ -444,7 +439,6 @@
 								return message;
 							}
 						});
-						reset();
 						close();
 					}}
 				>
