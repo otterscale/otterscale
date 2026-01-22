c1dc2dbf9bb4f6023d405354d312fd49ba7c67c6
tsaishawn <shawn_tsai@phison.com>
2026-01-22 17:29:07 +0800

    fix: edit chart values (#1443)
    * fix: edit chart values

--- web/src/lib/components/applications/store/commerce-store/chart-action-install-release.svelte
+++ web/src/lib/components/applications/store/commerce-store/chart-action-install-release.svelte
@@ -157,11 +157,13 @@
 				<Form.Label>
 					{m.configuration()}
 				</Form.Label>
-				<ReleaseValuesInputEdit
-					chartRef={versionReference}
-					bind:valuesYaml={request.valuesYaml}
-					bind:valuesMap={request.valuesMap}
-				/>
+				{#key request.chartRef}
+					<ReleaseValuesInputEdit
+						chartRef={request.chartRef}
+						bind:valuesYaml={request.valuesYaml}
+						bind:valuesMap={request.valuesMap}
+					/>
+				{/key}
 			</Form.Field>
 		</Form.Fieldset>
 
