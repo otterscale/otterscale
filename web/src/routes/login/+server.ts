3de41b707eb42ad29d01b650572411bf9379154a
Chung-Hsuan Tsai <paul_tsai@phison.com>
2026-01-27 11:14:08 +0800

    refactor(session): streamline cookie handling by introducing cookieName function and improving secure flag logic

--- web/src/routes/login/+server.ts
+++ web/src/routes/login/+server.ts
@@ -4,21 +4,26 @@ import { generateCodeVerifier, generateState } from 'arctic';
 import { keycloak } from '$lib/server/keycloak';
 import { isSecure } from '$lib/server/session';
 
-const COOKIE_OPTIONS = {
-	path: '/',
-	httpOnly: true,
-	secure: isSecure(),
-	maxAge: 60 * 10, // 10 minutes
-	sameSite: 'lax' as const
-};
-
 export async function GET(event: RequestEvent): Promise<Response> {
 	const state = generateState();
 	const codeVerifier = generateCodeVerifier();
 	const url = keycloak.createAuthorizationURL(state, codeVerifier, ['openid', 'profile', 'email']);
 
-	event.cookies.set('OS_STATE', state, COOKIE_OPTIONS);
-	event.cookies.set('OS_CODE_VERIFIER', codeVerifier, COOKIE_OPTIONS);
+	event.cookies.set('OS_STATE', state, {
+		httpOnly: true,
+		maxAge: 60 * 10, // 10 minutes
+		path: '/',
+		sameSite: 'lax' as const,
+		secure: isSecure()
+	});
+
+	event.cookies.set('OS_CODE_VERIFIER', codeVerifier, {
+		httpOnly: true,
+		maxAge: 60 * 10, // 10 minutes
+		path: '/',
+		sameSite: 'lax' as const,
+		secure: isSecure()
+	});
 
 	return new Response(null, {
 		status: 307,
