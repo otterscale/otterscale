885ac9b07ab17eec488b4d6d458254c8a3fcf2b8
Zhang, En-Yao <zhang_enyao@icloud.com>
2026-01-27 23:27:20 +0800

    fix: update type annotations for getResourceInspector function

--- web/src/lib/components/dynamical-table/inspectors/index.ts
+++ web/src/lib/components/dynamical-table/inspectors/index.ts
@@ -1,13 +1,15 @@
 import type { Component } from 'svelte';
 
+type InspectorProps = { object: any };
+
 import Default from './default.svelte';
 import Workspaces from './workspaces.svelte';
 
-function getResourceInspector(resource: string): Component | null {
+function getResourceInspector(resource: string): Component<InspectorProps> {
 	if (resource === 'workspaces') {
-		return Workspaces;
+		return Workspaces as Component<InspectorProps>;
 	}
-	return Default;
+	return Default as Component<InspectorProps>;
 }
 
 export { getResourceInspector };
