850fbf67de54cdc61caf41c22fd42bb6c1f19514
woody_lin <jj7158761@gmail.com>
2026-01-28 17:03:51 +0800

    fix(sheet-create-workspace): correct order of classes in layout

--- web/src/lib/components/layout/sheet-create-workspace.svelte
+++ web/src/lib/components/layout/sheet-create-workspace.svelte
@@ -15,7 +15,7 @@
 	<Sheet.Content class="inset-y-auto bottom-0 h-9/10 rounded-tl-lg sm:max-w-4/5">
 		<Sheet.Header class="h-full p-0">
 			<div class="flex h-full">
-				<div class="flex-1 p-6 overflow-y-auto">
+				<div class="flex-1 overflow-y-auto p-6">
 					<CreateWorkspaceForm onsuccess={handleClose} />
 				</div>
 				<!-- Workspace Image -->
