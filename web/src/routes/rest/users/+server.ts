a2bc4940a7de361427a38d03ec0b4a73af222f5e
woody_lin <jj7158761@gmail.com>
2026-01-28 16:28:43 +0800

    fix: API authentication checks

--- web/src/routes/rest/users/+server.ts
+++ web/src/routes/rest/users/+server.ts
@@ -1,10 +1,14 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 import { getUsers } from '$lib/server/users';
 
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, locals }) => {
+	if (!locals.session) {
+		error(401, 'Unauthorized');
+	}
+
 	const search = url.searchParams.get('search') || '';
 	const first = parseInt(url.searchParams.get('first') || '0', 10);
 	const max = parseInt(url.searchParams.get('max') || '10', 10);
