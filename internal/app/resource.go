5b27efed70373332b2fd6afc3d48727bd0306cfa
Paul Tsai <paul_tsai@phison.com>
2026-02-03 02:41:40 +0000

    refactor: move statusReasonToConnectCode map to package level for reuse in k8sErrorToConnectError function

--- internal/app/resource.go
+++ internal/app/resource.go
@@ -23,6 +23,28 @@ import (
 	"github.com/otterscale/otterscale/internal/core/resource"
 )
 
+var statusReasonToConnectCode = map[metav1.StatusReason]connect.Code{
+	metav1.StatusReasonUnauthorized:          connect.CodeUnauthenticated,
+	metav1.StatusReasonForbidden:             connect.CodePermissionDenied,
+	metav1.StatusReasonNotFound:              connect.CodeNotFound,
+	metav1.StatusReasonAlreadyExists:         connect.CodeAlreadyExists,
+	metav1.StatusReasonConflict:              connect.CodeFailedPrecondition,
+	metav1.StatusReasonGone:                  connect.CodeNotFound,
+	metav1.StatusReasonInvalid:               connect.CodeInvalidArgument,
+	metav1.StatusReasonServerTimeout:         connect.CodeDeadlineExceeded,
+	metav1.StatusReasonStoreReadError:        connect.CodeInternal,
+	metav1.StatusReasonTimeout:               connect.CodeDeadlineExceeded,
+	metav1.StatusReasonTooManyRequests:       connect.CodeResourceExhausted,
+	metav1.StatusReasonBadRequest:            connect.CodeInvalidArgument,
+	metav1.StatusReasonMethodNotAllowed:      connect.CodeUnimplemented,
+	metav1.StatusReasonNotAcceptable:         connect.CodeInvalidArgument,
+	metav1.StatusReasonRequestEntityTooLarge: connect.CodeResourceExhausted,
+	metav1.StatusReasonUnsupportedMediaType:  connect.CodeInvalidArgument,
+	metav1.StatusReasonInternalError:         connect.CodeInternal,
+	metav1.StatusReasonExpired:               connect.CodeInvalidArgument,
+	metav1.StatusReasonServiceUnavailable:    connect.CodeUnavailable,
+}
+
 type ResourceService struct {
 	pbconnect.UnimplementedResourceServiceHandler
 
@@ -313,28 +335,6 @@ func k8sErrorToConnectError(err error) error {
 	var statusErr *apierrors.StatusError
 	if !errors.As(err, &statusErr) {
 		return connect.NewError(connect.CodeInternal, err)
-	}
-
-	statusReasonToConnectCode := map[metav1.StatusReason]connect.Code{
-		metav1.StatusReasonUnauthorized:          connect.CodeUnauthenticated,
-		metav1.StatusReasonForbidden:             connect.CodePermissionDenied,
-		metav1.StatusReasonNotFound:              connect.CodeNotFound,
-		metav1.StatusReasonAlreadyExists:         connect.CodeAlreadyExists,
-		metav1.StatusReasonConflict:              connect.CodeFailedPrecondition,
-		metav1.StatusReasonGone:                  connect.CodeNotFound,
-		metav1.StatusReasonInvalid:               connect.CodeInvalidArgument,
-		metav1.StatusReasonServerTimeout:         connect.CodeDeadlineExceeded,
-		metav1.StatusReasonStoreReadError:        connect.CodeInternal,
-		metav1.StatusReasonTimeout:               connect.CodeDeadlineExceeded,
-		metav1.StatusReasonTooManyRequests:       connect.CodeResourceExhausted,
-		metav1.StatusReasonBadRequest:            connect.CodeInvalidArgument,
-		metav1.StatusReasonMethodNotAllowed:      connect.CodeUnimplemented,
-		metav1.StatusReasonNotAcceptable:         connect.CodeInvalidArgument,
-		metav1.StatusReasonRequestEntityTooLarge: connect.CodeResourceExhausted,
-		metav1.StatusReasonUnsupportedMediaType:  connect.CodeInvalidArgument,
-		metav1.StatusReasonInternalError:         connect.CodeInternal,
-		metav1.StatusReasonExpired:               connect.CodeInvalidArgument,
-		metav1.StatusReasonServiceUnavailable:    connect.CodeUnavailable,
 	}
 
 	if code, ok := statusReasonToConnectCode[statusErr.Status().Reason]; ok {
