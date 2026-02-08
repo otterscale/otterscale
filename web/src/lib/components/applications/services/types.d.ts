814cac0a08cd404dc7822658f814ecfc6a4a1296
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2026-02-09 00:53:57 +0800

    refactor: remove data table components and related services

--- web/src/lib/components/applications/services/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Application_Service } from '$lib/api/application/v1/application_pb';
-
-interface Service extends Application_Service {
-	hostname: string;
-}
-
-export type { Service };
