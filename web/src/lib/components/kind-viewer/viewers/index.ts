eee88e3d01cff2c2f3a95b0ece3e243539a070b2
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2026-02-09 01:20:07 +0800

    refactor: remove Pod viewer components and associated masks

--- web/src/lib/components/kind-viewer/viewers/index.ts
+++ web/src/lib/components/kind-viewer/viewers/index.ts
@@ -14,7 +14,6 @@ type ObjectMaskType = (object: any) => Record<string, JsonValue>;
 
 import { cronjobColumnDefinitions, cronjobFieldsMask, cronjobObjectMask } from './cronjob.js';
 import { defaultColumnDefinitions, defaultFieldsMask, defaultObjectMask } from './default.js';
-import { podColumnDefinitions, podFieldsMask, podObjectMask } from './pod.js';
 import {
 	resourceQuotaColumnDefinitions,
 	resourceQuotaFieldsMask,
@@ -25,8 +24,6 @@ function getFieldsGetter(kind: string): FieldsMaskType {
 	switch (kind) {
 		case 'CronJob':
 			return cronjobFieldsMask;
-		case 'Pod':
-			return podFieldsMask;
 		case 'ResourceQuota':
 			return resourceQuotaFieldsMask;
 		default:
@@ -38,8 +35,6 @@ function getObjectGetter(kind: string): ObjectMaskType {
 	switch (kind) {
 		case 'CronJob':
 			return cronjobObjectMask;
-		case 'Pod':
-			return podObjectMask;
 		case 'ResourceQuota':
 			return resourceQuotaObjectMask;
 		default:
@@ -51,8 +46,6 @@ function getColumnDefinitionsGetter(kind: string): ColumnDefinitionsType {
 	switch (kind) {
 		case 'CronJob':
 			return cronjobColumnDefinitions;
-		case 'Pod':
-			return podColumnDefinitions;
 		case 'ResourceQuota':
 			return resourceQuotaColumnDefinitions;
 		default:
