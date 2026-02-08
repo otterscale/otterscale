bb98f362b318dd71ced46fd22ffa4533a19e18f0
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2026-02-08 23:50:20 +0800

    chore: bump ModelArtifact version to 0.1.4

--- internal/core/versions/versions.go
+++ internal/core/versions/versions.go
@@ -4,7 +4,7 @@ package versions
 const (
 	GPUOperator   = "1.0.7" // HAMi 2.6.1 & GPU Operator 25.3.2
 	KubeVirtInfra = "0.1.2" // KubeVirt 1.6.2 & CDI 1.63.1
-	ModelArtifact = "0.1.3"
+	ModelArtifact = "0.1.4"
 	Registry      = "0.3.0"
 	SambaOperator = "0.1.0"
 )
