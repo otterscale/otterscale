46b39d4d679511f937806065a59b0537cdd0cd4b
Chung-Hsuan Tsai <paul_tsai@phison.com>
2026-09-18 21:33:16 +0800

    chore: update renovate configuration to support Go source dependencies (#296)

--- internal/providers/values/charts.go
+++ internal/providers/values/charts.go
@@ -5,12 +5,9 @@ import "github.com/otterscale/otterscale/internal/core"
 // The chart versions this build's rendered values are written for. Helm and
 // Flux both accept a semver range here, so "1.1.x" is a legitimate value.
 var chartVersions = core.ChartVersions{
-	// renovate: helm=otterscale-agent-flux registry=https://otterscale.github.io/helm-charts
-	AgentFlux: "1.1.x",
-	// renovate: helm=otterscale-agent registry=https://otterscale.github.io/helm-charts
-	Agent: "1.1.x",
-	// renovate: helm=flux registry=https://otterscale.github.io/helm-charts
-	Flux: "1.0.x",
+	AgentFlux: "1.1.x", // renovate: datasource=helm depName=otterscale-agent-flux registryUrl=https://otterscale.github.io/helm-charts
+	Agent:     "1.1.x", // renovate: datasource=helm depName=otterscale-agent registryUrl=https://otterscale.github.io/helm-charts
+	Flux:      "1.0.x", // renovate: datasource=helm depName=flux registryUrl=https://otterscale.github.io/helm-charts
 }
 
 func ProvideChartVersions() core.ChartVersions {
